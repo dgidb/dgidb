8c24c57529b44b56407c9c58a5c23f14c52f3096
James Koval <jkoval@genome.wustl.edu>
2012-05-10 16:49:49 -0500

    adding attributes hangoff on interactions

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,4 +2,5 @@ class Interaction < ActiveRecord::Base
     self.table_name = 'drug_gene_interaction_report'
     belongs_to :gene, foreign_key: :gene_name_report_id
     belongs_to :drug, foreign_key: :drug_name_report_id
+    has_many :interaction_attributes
 end
---
+++ app/models/interaction_attribute.rb
@@ -0,0 +1,4 @@
+class InteractionAttribute < ActiveRecord::Base
+    self.table_name = 'drug_gene_interaction_report_attribute'
+    belongs_to :interaction
+end
