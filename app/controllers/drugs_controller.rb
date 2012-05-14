ca241f882623d1b60c38342e5db8ee988767a55f
James Koval <jkoval@genome.wustl.edu>
2012-05-14 11:06:22 -0500

    Finished rendering data for drug view

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,5 +1,6 @@
 class DrugsController < ApplicationController
   def show
+    @title = params[:name]
     @drugs = Drug.where(name: params[:name])
   end
 end
--- app/models/drug.rb
+++ app/models/drug.rb
@@ -5,4 +5,17 @@ class Drug < ActiveRecord::Base
     has_many :interactions, foreign_key: :drug_name_report_id
     has_many :genes, through: :interactions
     belongs_to :citation
+
+    def original_data_source_url
+      base_url = self.citation.base_url
+      name = self.name
+      case self.citation.source_db_name
+      when 'DrugBank'
+        [base_url, 'drugs', name].join('/')
+      when 'TTD'
+        base_url + 'DRUG.asp?ID=' + name
+      else
+        base_url + name
+      end
+    end
 end
