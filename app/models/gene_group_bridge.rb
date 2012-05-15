d659deb8d9c4cd4e644e4cdaf04cfaae4261306b
James Koval <jkoval@genome.wustl.edu>
2012-05-15 10:52:11 -0500

    fix foreign keys in group bridge

--- app/models/gene_group_bridge.rb
+++ app/models/gene_group_bridge.rb
@@ -1,5 +1,5 @@
 class GeneGroupBridge < ActiveRecord::Base
     self.table_name = 'gene_name_group_bridge'
-    belongs_to :gene
-    belongs_to :gene_group
+    belongs_to :gene, foreign_key: :gene_name_report_id
+    belongs_to :gene_group, foreign_key: :gene_name_group_id
 end
