f003f8f949ebe0d29c50f60b023d1b23ca4b2515
James Koval <jkoval@genome.wustl.edu>
2012-05-18 15:26:18 -0500

    rid interactions controller of extra scoped variables

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -5,15 +5,13 @@ class InteractionsController < ApplicationController
     @gene = @interaction.gene
   end
   def interaction_search_results
-    @params = params
-    @geneNames = params[:genes].split("\n").collect(&:strip)
+    geneNames = params[:genes].split("\n").collect(&:strip)
     unless params[:geneFile].nil?
-      @geneNames.concat(params[:geneFile].read.split("\n")).collect(&:strip)
+      geneNames.concat(params[:geneFile].read.split("\n")).collect(&:strip)
     end
-    @geneNames.delete_if(&:empty?)
-    @geneGroups = DataModel::GeneGroup.where(name: @geneNames)
+    geneNames.delete_if(&:empty?)
 
-    search_results = LookupInteractions.find_groups_and_interactions(@geneNames)
+    search_results = LookupInteractions.find_groups_and_interactions(geneNames)
     @search_results = SearchResultsPresenter.new(search_results)
   end
 end
