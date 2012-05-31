e8d40e446c7c5fcae4dc381debf9c823871507e1
Adam Coffman <acoffman@genome.wustl.edu>
2012-05-31 16:12:58 -0500

    interaction scopes now fully working

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sqlite3'
 gem 'pg'
 gem 'less-rails-bootstrap'
 gem 'haml'
+gem 'squeel'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/data_model/interaction.rb
+++ app/models/data_model/interaction.rb
@@ -4,11 +4,6 @@ class DataModel::Interaction < ActiveRecord::Base
     belongs_to :drug, foreign_key: :drug_name_report_id
     has_many :interaction_attributes
     belongs_to :citation
-    with_hangoff_table :interaction_attributes, name_column: :name, value_column: :value
-
-    scope :basic,  includes(:interaction_attributes, drug: [:drug_categories])
-      .interaction_attributes(interaction_type: 'potentiator', and: {interaction_type: 'na', or: {is_known_action: 'yes'}})
-
 
     def types
       self.interaction_attributes.find_all{|attribute| attribute.name == 'interaction_type'}
@@ -18,13 +13,60 @@ class DataModel::Interaction < ActiveRecord::Base
       self.interaction_attributes.find_all{|attribute| attribute.name != 'interaction_type'}
     end
 
-    #drug.is_withdrawn=0,drug.is_nutraceutical=0,is_potentiator=0,(is_untyped=0 or is_known_action=1)
-    def self.basic
-      relation = includes(:interaction_attributes, drug: [:drug_categories])
-        .interaction_attributes(interaction_type: 'potentiator', and: {interaction_type: 'na', or: {is_known_action: 'yes'}})
-
-        #drug_name_report_id -> id
-      relation.merge(DataModel::Drug.drug_categories(drug_group: 'withdrawn', drug_category: 'nutraceutical'))
+    def self.attribute(name, val)
+      DataModel::Interaction.joins{interaction_attributes}.where{
+        (interaction_attributes.name.eq(name) & interaction_attributes.value.eq(val))
+      }.select{id}
     end
 
+    def self.drug_category(*val)
+      DataModel::Drug.joins{drug_categories}.where{
+        drug_categories.category_value.eq_any(val)
+      }.select{id}
+    end
+
+    def self.gene_name_alternate(val)
+      DataModel::Gene.joins{ gene_alternate_names }.where{
+        gene_alternate_names.alternate_name.like(val)
+      }
+    end
+
+    def self.basic
+    #'drug.is_withdrawn=0,drug.is_nutraceutical=0,is_potentiator=0,(is_untyped=0 or is_known_action=1)' if /default/;
+      DataModel::Interaction.joins{drug}.where{
+        id.not_in(my{attribute("interaction_type", "potentiator")}) &
+        ( id.not_in(my{attribute("interaction_type", "untyped")}) | id.in( my{attribute("is_known_action", "yes" )}) ) &
+        drug.id.not_in(my{drug_category("withdrawn","nutraceutical")})
+      }
+    end
+
+    def self.inhibitors_only
+    #'drug.is_withdrawn=0,drug.is_nutraceutical=0,is_potentiator=0,is_inhibitor=1,(is_untyped=0 or is_known_action=1)';
+      DataModel::Interaction.joins{drug}.where{
+        id.not_in(my{attribute("interaction_type", "potentiator")}) &
+        id.in(my{attribute("interaction_type", "inhibitor")}) &
+        ( id.not_in(my{attribute("interaction_type", "untyped")}) | id.in( my{attribute("is_known_action", "yes" )}) ) &
+        drug.id.not_in(my{drug_category("withdrawn","nutraceutical")})
+      }
+    end
+
+    def self.kinase_only
+    #'drug.is_withdrawn=0,drug.is_nutraceutical=0,is_potentiator=0,gene.is_kinase=1,(is_untyped=0 or is_known_action=1)'
+      DataModel::Interaction.joins{drug}.joins{gene}.where{
+        id.not_in(my{attribute("interaction_type", "potentiator")}) &
+        ( id.not_in(my{attribute("interaction_type", "untyped")}) | id.in( my{attribute("is_known_action", "yes" )}) ) &
+        drug.id.not_in(my{drug_category("withdrawn","nutraceutical")}) &
+        gene.id.in(my{gene_name_alternate("%kinase%")})
+      }
+    end
+
+    def self.anti_neoplastic_only
+    #'drug.is_withdrawn=0,drug.is_nutraceutical=0,is_potentiator=0,drug.is_antineoplastic=1,(is_untyped=0 or is_known_action=1)'
+      DataModel::Interaction.joins{drug}.joins{gene}.where{
+        id.not_in(my{attribute("interaction_type", "potentiator")}) &
+        id.in(my{attribute("interaction_type", "antineoplastic")}) &
+        ( id.not_in(my{attribute("interaction_type", "untyped")}) | id.in( my{attribute("is_known_action", "yes" )}) ) &
+        drug.id.not_in(my{drug_category("withdrawn","nutraceutical")})
+      }
+    end
 end
