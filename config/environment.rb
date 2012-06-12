1a07c7dc69420e635115814dd07fb2fd7336794e
Adam Coffman <acoffman@genome.wustl.edu>
2012-06-12 10:04:34 -0500

    use rails_root tmp dir for uploads instead of /tmp

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['TMPDIR'] = Rails.root.join('tmp/uploads').to_s
+
 # Initialize the rails application
 DruggableGene::Application.initialize!
