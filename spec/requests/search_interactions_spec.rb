5fab05c4d1813f297971479cd93f5b682025f09a
Jim Weible <jweible@genome.wustl.edu>
2012-06-20 15:04:55 -0500

    added a semi-useful test for search_interactions

--- spec/requests/search_interactions_spec.rb
+++ spec/requests/search_interactions_spec.rb
@@ -6,11 +6,23 @@ describe 'search_interactions' do
     page.status_code.should eq(200)
   end
 
-  it 'seems to work', :js => true do 
+  it 'searching FLT3 looks reasonable' do
     visit '/search_interactions'
-    click_button('defaultGenes')
+    fill_in('genes', :with => 'FLT3')
     click_button('Find Drug Interactions')
-    binding.pry
-    'hope for the best'
+    
+    within("#summary") do
+      page.should have_content("Search Terms: 1")
+      page.should have_content("Search Terms Matched Definitely: 1")
+      page.should have_content("Number Of Interactions For Definite Matches: 2")
+    end
+
+    within("#term_summary_tab") do
+      page.should have_table('', rows: [['FLT3', 'Definite', 'FLT3']]) 
+    end
+
+    within("#interaction_tab") do
+      page.should have_table('', rows: [['FLT3', 'FLT3', 'SORAFENIB', 'antagonist', 'DrugBank'], ['FLT3', 'FLT3', 'SUNITINIB', 'multitarget', 'DrugBank']])
+    end
   end
 end
