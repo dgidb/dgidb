30ac0a90fd145de03dcc73570743eb549cb54206
Jim Weible <jweible@genome.wustl.edu>
2012-06-21 15:11:28 -0500

    more tests

---
+++ spec/requests/search_druggable_gene_categories_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'search_druggable_gene_categories' do
+  before :each do
+    visit '/search_categories'
+  end
+
+  it 'looks reasonable' do
+    page.status_code.should eq(200)
+  end
+
+  it 'searching FLT3 looks reasonable' do
+    fill_in('genes', :with => 'FLT3')
+    select('KINASE', :from => 'categories')
+    select('TYROSINE KINASE', :from => 'categories')
+    select('CELL SURFACE', :from => 'categories')
+    click_button('Find Gene Categories')
+
+    within('#by_categories') do
+      page.should have_table('', rows: [['CELL SURFACE', '1', 'FLT3'], ['TYROSINE KINASE', '1', 'FLT3'], ['KINASE', '1', 'FLT3']]) 
+    end
+
+    within('#by_genes') do
+      page.should have_table('', rows: [['FLT3', 'CELL SURFACE', 'FLT3'], ['FLT3', 'TYROSINE KINASE', 'FLT3'], ['FLT3', 'KINASE', 'FLT3']])
+    end
+  end
+
+end
--- spec/requests/search_interactions_spec.rb
+++ spec/requests/search_interactions_spec.rb
@@ -49,4 +49,28 @@ describe 'search_interactions' do
       page.should have_table('', rows: [["FLT2","FGFR1","PALIFERMIN","n/a","DrugBank"]])
     end
   end
+
+  it 'searching with TTD only looks reasonable' do
+    fill_in('genes', :with => "FLT3\nFLT2\nFLT1")
+    uncheck('drugbank')
+    click_button('Find Drug Interactions')
+
+    within("#summary") do
+      page.should have_content("Search Terms: 3")
+      page.should have_content("Search Terms Matched Definitely: 3")
+      page.should have_content("Number Of Interactions For Definite Matches: 5")
+    end
+
+    within("#term_summary_tab") do
+      page.should have_table('', rows: [['FLT3', 'Definite', 'FLT3'], ['FLT2', 'Definite', 'FGFR1'], ['FLT1', 'Definite', 'FLT1']]) 
+    end
+
+    within("#interaction_tab") do
+      page.should have_table('', rows: [["FLT1","FLT1","SORAFENIB","inhibitor","TTD"], ["FLT1","FLT1","RANIBIZUMAB","inhibitor","TTD"]])
+      page.should have_table('', rows: [['FLT3', 'FLT3', 'SORAFENIB', 'antagonist', 'DrugBank'], ['FLT3', 'FLT3', 'SUNITINIB', 'multitarget', 'DrugBank'], ["FLT2","FGFR1","PALIFERMIN","n/a","DrugBank"]])
+    end
+  end
+
+  #TODO: write a .tsv test
+
 end
