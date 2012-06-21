222348b4cd20b0ddb11ad97ac4e361fe425a0033
Jim Weible <jweible@genome.wustl.edu>
2012-06-21 15:18:13 -0500

    test gene_categories filtering

--- spec/requests/search_druggable_gene_categories_spec.rb
+++ spec/requests/search_druggable_gene_categories_spec.rb
@@ -25,4 +25,21 @@ describe 'search_druggable_gene_categories' do
     end
   end
 
+  it 'filters work' do
+    fill_in('genes', :with => 'FLT3')
+    select('KINASE', :from => 'categories')
+    select('TYROSINE KINASE', :from => 'categories')
+    click_button('Find Gene Categories')
+
+    within('#by_categories') do
+      page.should have_table('', rows: [['TYROSINE KINASE', '1', 'FLT3'], ['KINASE', '1', 'FLT3']])
+      page.should have_table('', rows: [['CELL SURFACE', '1', 'FLT3']])
+    end
+
+    within('#by_genes') do
+      page.should have_table('', rows: [['FLT3', 'TYROSINE KINASE', 'FLT3'], ['FLT3', 'KINASE', 'FLT3']])
+      page.should have_table('', rows: [['FLT3', 'CELL SURFACE', 'FLT3']])
+    end
+  end
+
 end
