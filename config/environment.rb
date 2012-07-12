91a559cca90c66918a99af6da2f9acd1db33ab8f
Adam Coffman <acoffman@genome.wustl.edu>
2012-07-12 17:19:41 -0500

    create tmp parent directory on boot if it doesnt exist already

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 upload_path = Rails.root.join('tmp/uploads').to_s
 unless File.exists?(upload_path) && File.directory?(upload_path)
-  Dir.mkdir(upload_path)
+  FileUtils.mkdir_p(upload_path)
 end
 
 ENV['TMPDIR'] = upload_path
