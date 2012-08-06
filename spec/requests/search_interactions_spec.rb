55f31cf98483c521b63174b546e5590a9d383f4a
Jim Weible <jweible@genome.wustl.edu>
2012-08-06 15:17:36 -0500

    searching with TTD only looks reasonable also has an ambiguous result

--- spec/requests/search_interactions_spec.rb
+++ spec/requests/search_interactions_spec.rb
@@ -63,19 +63,19 @@ describe 'search_interactions' do
   end
 
   it 'searching with TTD only looks reasonable' do
-    fill_in('genes', :with => "FLT3\nFLT2\nFLT1")
-    #uncheck('drugbank')
+    fill_in('genes', :with => "FLT3\nFLT2\nFLT1\nSTK1")
     select('TTD', :from => 'sources')
     click_button('Find Drug Interactions')
 
     within("#summary") do
-      page.should have_content("Search Terms: 3")
+      page.should have_content("Search Terms: 4")
       page.should have_content("Search Terms Matched Definitely: 3")
+      page.should have_content("Search Terms Matched Ambiguously: 1")
       page.should have_content("Number Of Interactions For Definite Matches: 2")
     end
 
     within("#term_summary_tab") do
-      page.should have_table('', rows: [['FLT3', 'Definite', 'FLT3'], ['FLT1', 'Definite', 'FLT1'], ['FLT2', 'Definite', 'FGFR1']]) 
+      page.should have_table('', rows: [['FLT3', 'Definite', 'FLT3'], ['FLT2', 'Definite', 'FGFR1'], ['FLT1', 'Definite', 'FLT1']]) 
     end
 
     within("#interaction_tab") do
