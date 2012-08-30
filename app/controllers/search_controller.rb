21f909fede3a708195816c8387e13e42b53b4000
Adam Coffman <acoffman@genome.wustl.edu>
2012-08-30 13:06:10 -0500

    clean up search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,6 @@
 class SearchController < ApplicationController
   def search_results
     @search_active = "active"
-    start_time = Time.now
-    #do any validation here
-
-    search_results = Search.search(params)
-    @search_results = SearchResultsPresenter.new(search_results, params, start_time)
+    @search_results = Search.search(params[:search_term])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def tx(fragment_id)
     EXTERNAL_STRINGS[params["action"]][fragment_id].html_safe
   end
+
+  def to(fragment_id)
+    EXTERNAL_STRINGS[params["action"]][fragment_id]
+  end
 end
