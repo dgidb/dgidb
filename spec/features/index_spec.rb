0b1d66995fdae2280333b40d68b52f6daa965895
Adam Coffman <acoffman@genome.wustl.edu>
2012-12-04 19:53:40 -0600

    remove index spec since we dont have an index

--- spec/features/index_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'index' do
-  it 'check to see if the page exists' do
-    visit '/'
-    page.status_code.should eq(200)
-    page.should have_content('DGIDB')
-  end
-end
