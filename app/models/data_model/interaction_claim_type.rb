e35d5cd1e680d0e3a884597cc6fa6aae8bb9453e
Adam Coffman <acoffman@genome.wustl.edu>
2012-12-12 18:48:26 -0600

    override table inheritance auto typing

--- app/models/data_model/interaction_claim_type.rb
+++ app/models/data_model/interaction_claim_type.rb
@@ -1,6 +1,7 @@
 module DataModel
   class InteractionClaimType < ::ActiveRecord::Base
     include Genome::Extensions::UUIDPrimaryKey
+    self.inheritance_column = nil
 
     has_and_belongs_to_many :interaction_claims
   end
