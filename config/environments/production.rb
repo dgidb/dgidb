8875fcf3249742e31c7790e9db4d93687e600c1e
Adam Coffman <acoffman@genome.wustl.edu>
2013-02-12 14:26:03 -0600

    more asset tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ DruggableGene::Application.configure do
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
+  config.assets.manifest = Rails.root.join('public/assets')
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -48,6 +48,7 @@ DruggableGene::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile +=  %w( *.js *.css *.coffee *.less )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
