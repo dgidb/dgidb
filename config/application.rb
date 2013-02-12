e83b3fb7a8adb7db54909f06fd44a450d0068cf8
Adam Coffman <acoffman@genome.wustl.edu>
2013-02-12 12:37:03 -0600

    dont init the app when compiling assets

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module DruggableGene
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    #don't try to hit the db when compiling assets
+    config.assets.initialize_on_precompile = false
+
     # Custom directories with classes and modules you want to be autoloadable.
     #config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
