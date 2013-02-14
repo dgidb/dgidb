70f65a526a51c1fad8a0918c3b8c3fa8cec2c9ba
Adam Coffman <acoffman@genome.wustl.edu>
2013-02-14 14:36:06 -0600

    cache data source summary queries

--- app/models/data_sources.rb
+++ app/models/data_sources.rb
@@ -1,5 +1,11 @@
 class DataSources
 
+  include Genome::Extensions::HasCacheableQuery
+
+  cache_query :uniq_source_names_with_gene_claims, :uniq_source_names_with_gene_claims
+  cache_query :uniq_source_names, :all_uniq_source_names
+  cache_query :all_source_summaries, :all_source_summaries
+
   def self.uniq_source_names_with_gene_claims
     uniq_source_names_with_table(:gene_claims)
   end
