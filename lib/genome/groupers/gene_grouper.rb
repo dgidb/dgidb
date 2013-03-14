a6fd7b04086d946f1bfd1ba5bd34fee35fd750e0
Adam Coffman <acoffman@genome.wustl.edu>
2013-03-14 10:28:51 -0500

    fix formatting a little

--- lib/genome/groupers/gene_grouper.rb
+++ lib/genome/groupers/gene_grouper.rb
@@ -1,30 +1,30 @@
 module Genome
   module Groupers
     class GeneGrouper
-      alt_to_entrez = Hash.new() {|hash, key| hash[key] = []}
-      alt_to_other = Hash.new() {|hash, key| hash[key] = []}
+      @alt_to_entrez = Hash.new() {|hash, key| hash[key] = []}
+      @alt_to_other = Hash.new() {|hash, key| hash[key] = []}
 
-      def self.run()
-        load
+      def self.run
+        preload
         create_groups
         add_members
       end
 
-      def load
+      def self.preload
         DataModel::GeneClaimAlias.each do |gca|
           gene_claim_alias = gca.alias
           next if gene_claim_alias =~ /^.$/
-          next if gene_claim_alias =~ /^\d\d$/
+            next if gene_claim_alias =~ /^\d\d$/
 
-          if(gca.nomenclature == 'Gene Symbol' and gca.gene_claim.citation.source_db_name == 'Entrez')
-            alt_to_entrez[gene_claim_alias].push(gca)
-          else
-            alt_to_other[gene_claim_alias].push(gca)
-          end  
+            if(gca.nomenclature == 'Gene Symbol' and gca.gene_claim.citation.source_db_name == 'Entrez')
+              alt_to_entrez[gene_claim_alias].push(gca)
+            else
+              alt_to_other[gene_claim_alias].push(gca)
+            end
         end
       end
 
-      def create_groups
+      def self.create_groups
         alt_to_entrez.each_key do |key|
           gene_claims = alt_to_entrez[key].map{|a| a.gene_claim}
           gene = DataModel::Gene.where(name: key)
@@ -35,42 +35,38 @@ module Genome
           else
             DataModel::Gene.new.tap do |g|
               g.name = key,
-              g.long_name = '', #TODO: fill me in
-              g.gene_claims = gene_claims
+                g.long_name = '', #TODO: fill me in
+                g.gene_claims = gene_claims
             end
           end
         end
       end
 
-     def add_members
-       DataModel::GeneClaim.all.each do |gene_claim|
-         next unless gene_claim.genes.empty?
-         indirect_groups = Hash.new
-         direct_groups = Hash.new
+      def self.add_members
+        DataModel::GeneClaim.all.each do |gene_claim|
+          next unless gene_claim.genes.empty?
+          indirect_groups = Hash.new { |h, k| h[k] = 0 }
+          direct_groups = Hash.new { |h, k| h[k] = 0 }
 
-         direct_groups[gene_claim.name]++ if DataModel::Gene.where(name: gene_claim.name).any?
-         gene_claim.gene_claim_alias.each do |gene_claim_alias|
-           direct_groups[gene_claim_alias.alias]++ if DataModel::Gene.where(name: gene_claim_alias.alias).any?
-           alt_genes = alt_to_other[gene_claim_alias].map{|a| a.gene_claim}
-           alt_genes.each do |alt_gene|
-             indirect_name = alt_gene.genes.map{|g| g.name} #THIS IS BAD, M'KAY
-             indirect_group[indirect_name]++ if indirect_name
-           end
+          direct_groups[gene_claim.name] += 1 if DataModel::Gene.where(name: gene_claim.name).any?
+          gene_claim.gene_claim_alias.each do |gene_claim_alias|
+            direct_groups[gene_claim_alias.alias] +=1 if DataModel::Gene.where(name: gene_claim_alias.alias).any?
+            alt_genes = alt_to_other[gene_claim_alias].map{|a| a.gene_claim}
+            alt_genes.each do |alt_gene|
+              indirect_name = alt_gene.genes.map{|g| g.name} #THIS IS BAD, M'KAY
+              indirect_group[indirect_name] += 1 if indirect_name
+            end
 
-           if direct_groups.keys.length == 1 
-             gene = DataModel::Gene.where(name: direct_groups.keys.first).first
-             gene.gene_claims.push(gene_claim)
-           elsif direct_groups.keys.length == 0 && indirect_groups.keys.length == 1
-             gene = DataModel::Gene.where(name: indirect_groups.key.first).first
-             gene.gene_claims.push(gene_claim)
-           end
-         end
-       end
-     end
-
+            if direct_groups.keys.length == 1
+              gene = DataModel::Gene.where(name: direct_groups.keys.first).first
+              gene.gene_claims.push(gene_claim)
+            elsif direct_groups.keys.length == 0 && indirect_groups.keys.length == 1
+              gene = DataModel::Gene.where(name: indirect_groups.key.first).first
+              gene.gene_claims.push(gene_claim)
+            end
+          end
+        end
+      end
     end
   end
-end
-end
-end
 end
