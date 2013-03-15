6091943b99eac1043f6959edb0c8a04c25c4f60a
Adam Coffman <acoffman@genome.wustl.edu>
2013-03-15 16:35:19 -0500

    source column hack - needs work, ignores filter

--- app/presenters/gene_category_search_result_presenter.rb
+++ app/presenters/gene_category_search_result_presenter.rb
@@ -1,13 +1,30 @@
 class GeneCategorySearchResultPresenter
   include Genome::Extensions
-  attr_accessor :search_term, :gene_category, :gene_name, :gene_display_name, :gene, :sources
+  attr_accessor :search_term, :gene_category, :gene_name, :gene_display_name, :gene
   def initialize(gene_category, search_term, gene_name, gene_display_name, gene)
     @gene_category = gene_category
     @search_term = search_term
     @gene_name = gene_name
     @gene_display_name = gene_display_name
     @gene = gene
-    @sources = sources
+  end
+
+  def sources(context)
+    sources_with_category
+      .map do |source|
+        context.instance_exec do
+          link_to source.source_db_name, source_path(source.source_db_name)
+        end
+      end
+      .join(', ').html_safe
+  end
+
+  private
+  def sources_with_category
+    @gene.gene_claims.select do |gc|
+      gc.gene_claim_categories.map(&:name)
+      .include?(gene_category.upcase)
+    end.map { |gc| gc.source }
   end
 end
 
