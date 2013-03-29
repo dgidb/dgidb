07404196655daa8067bf32f1045b0f18a167ec61
Adam Coffman <acoffman@genome.wustl.edu>
2013-03-29 16:31:26 -0500

    add source trust level to eager load statement

--- app/models/data_model/gene_claim_alias.rb
+++ app/models/data_model/gene_claim_alias.rb
@@ -4,7 +4,7 @@ module DataModel
     belongs_to :gene_claim
 
     def self.for_search
-      eager_load(gene_claim: [genes: [gene_claims: [interaction_claims: [:source, :drug_claim, :interaction_claim_types, gene_claim: [genes: [gene_claims: [:gene_claim_categories]]]]]]])
+      eager_load(gene_claim: [genes: [gene_claims: {interaction_claims: { source: [:source_trust_level], drug_claim: [:source], interaction_claim_types: [], gene_claim: [genes: [gene_claims: [:gene_claim_categories]]]}}]])
     end
 
     def self.for_gene_categories
