b53df4d328dbf06cd9a1377dd51a731c65ad3ae3
Adam Coffman <acoffman@genome.wustl.edu>
2013-03-29 15:41:52 -0500

    no more hardcoded source names in css

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,14 +1,14 @@
 class SourcesController < ApplicationController
   caches_page :sources, :show
   def show
-    @source = DataModel::Source.where('source_db_name ILIKE ?', params[:source_db_name]).first
+    @source = DataModel::Source.eager_load(:source_type).where('source_db_name ILIKE ?', params[:source_db_name]).first
     !@source.blank? || not_found("This source doesn't exist in our system!")
     @title = @source.source_db_name
   end
 
   def sources
     @help_active = 'active'
-    @sources = DataModel::Source.all
+    @sources = DataModel::Source.eager_load(:source_type).all
   end
 
 end
