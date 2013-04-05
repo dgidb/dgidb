df59a4d27bf3697204318277a19061bfbcb5022e
Adam Coffman <acoffman@genome.wustl.edu>
2013-04-05 16:24:22 -0500

    update helper spec to test new methods

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe ApplicationHelper, '#dynamic_link_to' do
-
   it "appends an image icon to an external link" do
     url = 'http://www.google.com'
     title = 'outside'
@@ -24,7 +23,6 @@ describe ApplicationHelper, '#ext_link_to' do
 end
 
 describe ApplicationHelper, '#icon' do
-
   before :each do
     @flag_content = 'flag content'
   end
@@ -44,12 +42,9 @@ describe ApplicationHelper, '#icon' do
   it "merges class attributes correctly" do
     helper.icon( :flag, class: 'test' ).should eq helper.content_tag( :i, nil, class: ['test', 'icon-flag'] )
   end
-
 end
 
-
 describe ApplicationHelper, '#tx' do
-
   before :each do
     @test_text = 'test'
     @test_key = 'text'
@@ -65,12 +60,9 @@ describe ApplicationHelper, '#tx' do
   it "should return an html_safe string (unescaped)" do
     helper.tx( @test_key ).should be_html_safe
   end
-
 end
 
-
 describe ApplicationHelper, '#to' do
-
   before :each do
     @test_obj = [ 1, 2 ,3 ]
     @test_key = 'text'
@@ -82,5 +74,26 @@ describe ApplicationHelper, '#to' do
   it "should lookup the correct hash value based on action and key" do
     helper.to( @test_key ).should eq @test_obj
   end
+end
 
+describe ApplicationHelper, '#label' do
+  it 'should create a span with <label-given_class> and label as the classes' do
+    helper.label('this is a success label').should  eq('<span class="label label-success">this is a success label</span>')
+    helper.label('this is a warning label', 'warning').should  eq('<span class="label label-warning">this is a warning label</span>')
+  end
+
+  it 'should create html safe content' do
+    helper.label('this is a success label').should  be_html_safe
+  end
+end
+
+describe ApplicationHelper, '#unread_news_badge' do
+  it 'should create the appropriate span if given true' do
+    helper.unread_news_badge(true).should eq('<span class="badge badge-important">!</span>')
+  end
+
+  it 'should create nothing if given false or default' do
+    helper.unread_news_badge(false).should eq(nil)
+    helper.unread_news_badge.should eq(nil)
+  end
 end
