14045f19c4729e21c6f44b1d8a4ff302f4ff26d2
Adam Coffman <acoffman@genome.wustl.edu>
2013-04-18 14:32:41 -0500

    dynamic filtering of sources on browse categories page and scoped individual category pages

--- spec/searches/lookup_categories_spec.rb
+++ spec/searches/lookup_categories_spec.rb
@@ -1,66 +1,94 @@
 require 'spec_helper'
 
 describe LookupCategories do
-  describe '::find_genes_for_category' do
+  describe '::find_genes_for_category_and_sources' do
 
-    it 'should find all genes for the given category' do
+    it 'should find all genes for the given category and sources' do
+      source = Fabricate(:source)
       category = Fabricate(:gene_claim_category)
       gene_claims = (1..3).map do
-        Fabricate(:gene_claim, gene_claim_categories: [category], source: Fabricate(:source))
+        Fabricate(:gene_claim, gene_claim_categories: [category], source: source)
       end
 
       genes = gene_claims.map { |gc| Fabricate(:gene, gene_claims: [gc]) }
-      found_genes = LookupCategories.find_genes_for_category(category.name)
+      found_genes = LookupCategories.find_genes_for_category_and_sources(category.name, [source.source_db_name])
       genes.sort.should eq(found_genes.sort)
     end
 
     it 'should only find unique genes' do
       #multiple gene claims in the same category mapped to same gene group
+      source = Fabricate(:source)
       category = Fabricate(:gene_claim_category)
       gene_claims = (1..3).map do
-        Fabricate(:gene_claim, gene_claim_categories: [category], source: Fabricate(:source))
+        Fabricate(:gene_claim, gene_claim_categories: [category], source: source)
       end
 
       gene = Fabricate(:gene, gene_claims: gene_claims)
-      found_gene = LookupCategories.find_genes_for_category(category.name)
+      found_gene = LookupCategories.find_genes_for_category_and_sources(category.name, [source.source_db_name])
 
       Array(gene).should eq(found_gene)
     end
 
     it 'should only find genes in the selected category' do
+      source = Fabricate(:source)
       categories = (1..2).map { Fabricate(:gene_claim_category) }
 
       gene_claims = (1..2).map do
-        Fabricate(:gene_claim, gene_claim_categories: [categories.first], source: Fabricate(:source))
+        Fabricate(:gene_claim, gene_claim_categories: [categories.first], source: source)
       end
 
       gene_claim_in_correct_category = Fabricate(:gene_claim, gene_claim_categories: [categories.last],
-                                                 source: Fabricate(:source))
+                                                 source: source)
 
       gene_in_correct_category = Fabricate(:gene, gene_claims: [gene_claim_in_correct_category, gene_claims.first])
       gene_in_incorrect_category = Fabricate(:gene, gene_claims: gene_claims)
 
 
-      found_genes = LookupCategories.find_genes_for_category(categories.last.name)
+      found_genes = LookupCategories.find_genes_for_category_and_sources(categories.last.name, [source.source_db_name])
 
       found_genes.include?(gene_in_correct_category).should be_true
       found_genes.include?(gene_in_incorrect_category).should be_false
     end
+
+    it 'should only find genes in the selected source' do
+      source1 = Fabricate(:source)
+      source2 = Fabricate(:source)
+      category = Fabricate(:gene_claim_category)
+      gene_claims_in_source_and_category = (1..3).map do
+        Fabricate(:gene_claim, gene_claim_categories: [category], source: source1)
+      end
+
+      #create some gene claims in our category but not from this source
+      gene_claims_not_in_source = (1..3).map do
+        Fabricate(:gene_claim, gene_claim_categories: [category], source: source2)
+      end
+
+      genes_to_find = gene_claims_in_source_and_category.map { |gc| Fabricate(:gene, gene_claims: [gc]) }
+      gene_claims_not_in_source.each { |gc| Fabricate(:gene, gene_claims: [gc]) }
+      found_genes = LookupCategories.find_genes_for_category_and_sources(category.name, [source1.source_db_name])
+      genes_to_find.sort.should eq(found_genes.sort)
+    end
   end
 
-  describe '::get_uniq_category_names_with_counts' do
+  describe '::get_category_names_with_counts_with_sources' do
 
     it 'should return both the category names and the correct count of gene claims in that category' do
       categories = (1..3).map { Fabricate(:gene_claim_category) }.sort_by { |c| c.name }
 
-      (1..3).each { |i| Fabricate(:gene_claim, gene_claim_categories: categories.take(i)) }
+      source1 = Fabricate(:source)
+      source2 = Fabricate(:source)
 
-      categories_with_counts = LookupCategories.get_uniq_category_names_with_counts
+      (1..3).each { |i| Fabricate(:gene_claim, source: source1, gene_claim_categories: categories.take(i)) }
+
+      categories_with_counts = LookupCategories.get_category_names_with_counts_in_sources(source1.source_db_name)
 
       categories_with_counts.each_with_index do |category, i|
         categories[i].name.should eq(category.name)
         categories[i].gene_claims.count.should eq(category.gene_count.to_i)
       end
+
+      empty_results = LookupCategories.get_category_names_with_counts_in_sources(source2.source_db_name)
+      empty_results.size.should eq(0)
     end
 
   end
