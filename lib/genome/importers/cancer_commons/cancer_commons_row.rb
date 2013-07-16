9228f5b382800f54c36ae101e31239e38708a4bd
Adam Coffman <acoffman@genome.wustl.edu>
2013-07-16 15:57:41 -0500

    update cancercommons importer to make multiple gene claim aliases

--- lib/genome/importers/cancer_commons/cancer_commons_row.rb
+++ lib/genome/importers/cancer_commons/cancer_commons_row.rb
@@ -5,7 +5,7 @@ module Genome
       class CancerCommonsRow < Genome::Importers::DelimitedRow
         attribute :primary_gene_name
         attribute :entrez_gene_id
-        attribute :reported_gene_name
+        attribute :reported_gene_names, Array
         attribute :pubchem_drug_id
         attribute :pubchem_drug_name
         attribute :source_reported_drug_name
