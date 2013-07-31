4a445e653ef4c1f19f88b8ad4737555e2ec4c70e
Adam Coffman <acoffman@genome.wustl.edu>
2013-07-31 15:15:15 -0500

    update sql logging method

--- lib/utils/logging.rb
+++ lib/utils/logging.rb
@@ -1,15 +1,12 @@
 module Utils
   module Logging
-    def self.toggle_sql
-      if @old_logger
-        puts 'Enabling ActiveRecord logging...'
-        ActiveRecord::Base.logger = @old_logger
-        @old_logger = nil
-      else
-        puts 'Disabling ActiveRecord logging...'
-        @old_logger = ActiveRecord::Base.logger
-        ActiveRecord::Base.logger = nil
-      end
+    def self.without_sql
+      puts 'Disabling ActiveRecord logging...'
+      old_logger = ActiveRecord::Base.logger
+      ActiveRecord::Base.logger = nil
+      yield
+      puts 'Enabling ActiveRecord logging...'
+      ActiveRecord::Base.logger = old_logger
     end
   end
 end
