10fc341f18e461280a3913f84e9dc15a62ac30af
Adam Coffman <acoffman@genome.wustl.edu>
2013-08-01 18:33:15 -0500

    allow post requests for related genes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ DruggableGene::Application.routes.draw do
   match 'cache/invalidate' => 'utilities#invalidate_cache'
   post  'download_table' => 'utilities#download_request_content'
   post  'api/v1/interactions' => 'services_v1#interactions'
+  post  'api/v1/related_genes' => 'services_v1#related_genes'
   get   'api/v1/:action' => 'services_v1#:action'
   match 'interaction_search_results' => 'interaction_claims#interaction_search_results'
   match 'interactions_for_related_genes' => 'interaction_claims#interactions_for_related_genes'
