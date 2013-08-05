e44d55330e6e83abf98b60d56bace3401e084bc7
Adam Coffman <acoffman@genome.wustl.edu>
2013-08-05 15:18:52 -0500

    toggle logging back on in ensure block

--- lib/utils/logging.rb
+++ lib/utils/logging.rb
@@ -5,8 +5,9 @@ module Utils
       old_logger = ActiveRecord::Base.logger
       ActiveRecord::Base.logger = nil
       yield
-      puts 'Enabling ActiveRecord logging...'
-      ActiveRecord::Base.logger = old_logger
+      ensure
+        puts 'Enabling ActiveRecord logging...'
+        ActiveRecord::Base.logger = old_logger
     end
   end
 end
