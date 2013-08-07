8c828fcc1644cf877fea5941b729563a4a78a724
Adam Coffman <acoffman@genome.wustl.edu>
2013-08-07 09:44:59 -0500

    make drug name case insensitive

--- app/controllers/drug_claims_controller.rb
+++ app/controllers/drug_claims_controller.rb
@@ -1,7 +1,7 @@
 class DrugClaimsController < ApplicationController
   def show
     @drug_claim = DrugClaimPresenter.new DataModel::DrugClaim.for_show
-      .where(name: params[:name])
+      .where(name: params[:name].upcase)
       .where('sources.source_db_name' => params[:source_db_name])
       .first
   end
