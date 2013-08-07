e3b518a24754f11d85126278c51977e6d219f572
Adam Coffman <acoffman@genome.wustl.edu>
2013-08-07 09:46:21 -0500

    make gene claim path case insensitive

--- app/controllers/gene_claims_controller.rb
+++ app/controllers/gene_claims_controller.rb
@@ -1,10 +1,8 @@
 class GeneClaimsController < ApplicationController
   def show
-    name_param = params[:name]
-    source_db_name_param = params[:source_db_name]
     @gene_claim = GeneClaimPresenter.new DataModel::GeneClaim.for_show
-      .where(name: name_param)
-      .where('sources.source_db_name' => source_db_name_param)
+      .where(name: params[:name].upcase)
+      .where('sources.source_db_name' => params[:source_db_name])
       .first
   end
 end
