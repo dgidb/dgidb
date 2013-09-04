6775c01a9c8dc38ee7aaef829021877cf7495b52
Adam Coffman <acoffman@genome.wustl.edu>
2013-09-04 14:37:23 -0500

    throw a 404 instead of a 500 for not found drug claims

--- app/controllers/drug_claims_controller.rb
+++ app/controllers/drug_claims_controller.rb
@@ -1,8 +1,10 @@
 class DrugClaimsController < ApplicationController
   def show
-    @drug_claim = DrugClaimPresenter.new DataModel::DrugClaim.for_show
+    result = DataModel::DrugClaim.for_show
       .where(name: params[:name].upcase)
       .where('sources.source_db_name' => params[:source_db_name])
-      .first
+      .first || not_found("No drug claim from #{params[:source_db_name]} with name #{params[:name]} found!")
+
+      @drug_claim = DrugClaimPresenter.new(result)
   end
 end
