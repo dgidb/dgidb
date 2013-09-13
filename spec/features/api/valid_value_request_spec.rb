a6f5f499066a172f1820b8b69c65aaf57fa66c97
Adam Coffman <acoffman@genome.wustl.edu>
2013-09-13 16:54:59 -0500

    add api specs for valid value calls

---
+++ spec/features/api/valid_value_request_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe 'api endpoints that request valid enumerations' do
+  def generate_and_check_data(path, type_name, compare_column)
+      objs = (1..3).map { Fabricate(type_name) }
+
+      visit path
+      page.status_code.should eq(200)
+      body = JSON.parse(page.body)
+      body.should be_an_instance_of(Array)
+      body.sort.should eq objs.map(&compare_column).sort
+  end
+
+  describe 'gene_categories' do
+    it 'should return a list of all current gene category names as json' do
+      generate_and_check_data('/api/v1/gene_categories.json',
+                              :gene_claim_category,
+                              :name)
+    end
+  end
+
+  describe 'drug_types' do
+    it 'should return a list of all drug type names in the system as json' do
+      generate_and_check_data('/api/v1/drug_types.json',
+                              :drug_claim_type,
+                              :type)
+    end
+  end
+
+  describe 'interaction_types' do
+    it 'should return a list of all interaction type names in the system as json' do
+      generate_and_check_data('/api/v1/interaction_types.json',
+                              :interaction_claim_type,
+                              :type)
+    end
+  end
+
+  describe 'interaction_sources' do
+    it 'should return a list of all sources that provide interaction claims' do
+      interaction_source_type = Fabricate(:source_type, type: 'interaction')
+      source = Fabricate(:source, source_type: interaction_source_type)
+      non_interaction_source_type = Fabricate(:source_type, type: 'turkey')
+      non_interaction_source = Fabricate(:source, source_type: non_interaction_source_type)
+
+      visit '/api/v1/interaction_sources.json'
+
+      page.status_code.should eq(200)
+      body = JSON.parse(page.body)
+
+      body.should be_an_instance_of(Array)
+      body.should include(source.source_db_name)
+
+      body.should_not include(non_interaction_source.source_db_name)
+    end
+  end
+
+  describe 'source_trust_levels' do
+    it 'should return a list of all source trust levels in the system as json' do
+      generate_and_check_data('/api/v1/source_trust_levels.json',
+                              :source_trust_level,
+                              :level)
+    end
+  end
+
+end
