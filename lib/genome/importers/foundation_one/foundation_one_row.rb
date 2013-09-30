7abcf027e47994022b9ea7ed6d54e4e11e886b89
Nick Spies <nspies@dhcp-10-0-108-114.gsc.wustl.edu>
2013-09-30 16:17:26 -0500

    Add foundation one importer draft

---
+++ lib/genome/importers/foundation_one/foundation_one_row.rb
@@ -0,0 +1,13 @@
+require 'genome/importers/delimited_row'
+module Genome
+	module Importers
+		module FoundationOne
+			class FoundationOne < Genome::Importers::DelimitedRow
+				attribute :reported_gene_name
+				attribute :entrez_gene_name
+				attribute :entrez_gene_id
+				attribute :gene_category
+			end
+		end
+	end
+end
---
+++ lib/genome/importers/foundation_one/foundation_one_tsv_importer.rb
@@ -0,0 +1,31 @@
+module Genome
+	module Importers
+		module FoundationOne
+
+			def self.source_info
+			{
+				base_url: 'http://www.foundationone.com/help/current-gene-list.php',
+				citation: 'insert-citation-here',
+				site_url: 'http://www.foundationone.com/',
+				source_db_version: '20-Sept-2013',
+				source_type_id: DataModel::SourceType.INTERACTION,
+				source_trust_level_id: DataModel::SourceTrustLevel.EXPERT_CURATED,
+				source_db_name: 'Foundation One',
+				full_name: 'Foundation One',
+			}
+			end
+
+			def self.run(tsv_path)
+				TSVImporter.import tsv_path, FoundationOneRow, source_info do 
+					interaction known_action_type: 'unknown' do 
+						gene :entrez_gene_name, nomenclature: 'Entrez Gene Name' do
+							attribute :reported_gene_name, nomenclature: 'Source Reported Gene Name'
+							name :entrez_gene_id, nomenclature: "Entrez Gene ID"
+							attribute :gene_category, nomenclature: "Gene Category" 
+						end
+					end
+				end.save!
+			end
+		end
+	end
+end
