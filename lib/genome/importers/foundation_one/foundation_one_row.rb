af8f30cf855299e4f56c76d346b8803cc12bdb55
Adam Coffman <acoffman@genome.wustl.edu>
2013-10-09 17:55:00 -0500

    update foundation one importer to reflect that it is a category source rather than an interaction one

--- lib/genome/importers/foundation_one/foundation_one_row.rb
+++ lib/genome/importers/foundation_one/foundation_one_row.rb
@@ -1,13 +1,13 @@
 require 'genome/importers/delimited_row'
 module Genome
-	module Importers
-		module FoundationOneRow
-			class FoundationOne < Genome::Importers::DelimitedRow
-				attribute :reported_gene_name
-				attribute :entrez_gene_name
-				attribute :entrez_gene_id
-				attribute :gene_category
-			end
-		end
-	end
+  module Importers
+    module FoundationOne
+      class FoundationOneRow < Genome::Importers::DelimitedRow
+        attribute :reported_gene_name
+        attribute :entrez_gene_name
+        attribute :entrez_gene_id
+        attribute :gene_category
+      end
+    end
+  end
 end
