399568c43c3b0044843b47d1e93c8a809cd59713
Malachi Griffith <mgriffit@genome.wustl.edu>
2013-10-14 17:16:35 -0500

    Added citation for foundation one interactions

--- lib/genome/importers/foundation_one_interactions/foundation_one_interactions_tsv_importer.rb
+++ lib/genome/importers/foundation_one_interactions/foundation_one_interactions_tsv_importer.rb
@@ -5,7 +5,7 @@ module Genome
             def self.source_info
             {
                 base_url: 'http://www.foundationone.com/help/current-gene-list.php',
-                citation: 'insert-citation-here',
+                citation: 'High-throughput detection of actionable genomic alterations in clinical tumor samples by targeted, massively parallel sequencing. Wagle N, Berger MF, ..., Meyerson M, Gabriel SB, Garraway LA. Cancer Discov. 2012 Jan;2(1):82-93',
                 site_url: 'http://www.foundationone.com/',
                 source_db_version: '9-Oct-2013',
                 source_type_id: DataModel::SourceType.INTERACTION,
