8032dc05c4be5e5774a78291a22fad767ca184cb
Adam Coffman <acoffman@genome.wustl.edu>
2013-10-18 15:19:07 -0500

    update generic search to eager load appropriate fields

--- app/searches/search.rb
+++ app/searches/search.rb
@@ -1,26 +1,15 @@
 class Search
-  class << self
-    def search(search_term)
-      raise "You must specify a search term!" if search_term.blank?
-      search_term += ":*"
+  def self.search(search_term)
+    raise "You must specify a search term!" if search_term.blank?
+    search_term += ":*"
 
-      [].tap do |results|
-        results.concat DataModel::Gene.advanced_search(name: search_term)
-        results.concat DataModel::Drug.advanced_search(name: search_term)
-        results.concat DataModel::GeneClaimAlias.advanced_search(alias: search_term)
-        results.concat DataModel::DrugClaimAlias.advanced_search(alias: search_term)
-        results.concat DataModel::GeneClaim.advanced_search(name: search_term)
-        results.concat DataModel::DrugClaim.advanced_search(name: search_term)
-
-        #TODO: these are ready to eager load the needed fields for the search result pages
-        #but eager loading doesn't play nice with texticle...
-        #results.concat DataModel::Gene.includes(:gene_claims).search_by_name(search_term)
-        #results.concat DataModel::Drug.includes(:drug_claims).search_by_name(search_term)
-        #results.concat DataModel::GeneClaimAlias.includes(gene_claim: [:source]).search_by_alias(search_term)
-        #results.concat DataModel::DrugClaimAlias.includes(drug_claim: [:source]).search_by_alias(search_term)
-        #results.concat DataModel::GeneClaim.includes(:source).search_by_name(search_term)
-        #results.concat DataModel::DrugClaim.includes(:source).search_by_name(search_term)
-      end.map{ |r| SearchResultPresenter.new(r) }
-    end
+    [].tap do |results|
+      results.concat DataModel::Gene.includes(:gene_claims).advanced_search(name: search_term)
+      results.concat DataModel::Drug.includes(:drug_claims).advanced_search(name: search_term)
+      results.concat DataModel::GeneClaimAlias.includes(gene_claim: [:source]).advanced_search(alias: search_term)
+      results.concat DataModel::DrugClaimAlias.includes(drug_claim: [:source]).advanced_search(alias: search_term)
+      results.concat DataModel::GeneClaim.includes(:source).advanced_search(name: search_term)
+      results.concat DataModel::DrugClaim.includes(:source).advanced_search(name: search_term)
+    end.map{ |r| SearchResultPresenter.new(r) }
   end
 end
---
+++ spec/searches/search_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Search do
+  before :each do
+    @entities = {
+        DataModel::Gene => :name,
+        DataModel::Drug => :name,
+        DataModel::GeneClaimAlias => :alias,
+        DataModel::DrugClaimAlias => :alias,
+        DataModel::GeneClaim => :name,
+        DataModel::DrugClaim => :name
+    }
+  end
+
+  it 'should use the textacular .advanced_search to leverage postgres full text search' do
+    @entities.keys.each do |model|
+      model.should_receive(:advanced_search).and_return([])
+    end
+    Search.search('turkey')
+  end
+
+  it 'should throw an exception if given a blank or nil input' do
+    expect { Search.search(nil) }.to raise_error
+    expect { Search.search('') }.to raise_error
+    expect { Search.search('  ') }.to raise_error
+  end
+
+  it 'should wrap results in SearchResultPresenter objects' do
+    Fabricate(:drug, name: 'turkey')
+    results = Search.search('turkey')
+
+    results.size.should eq(1)
+    results.first.should be_a(SearchResultPresenter)
+  end
+
+  it 'should search the correct field for each entity type' do
+    search_term = 'test'
+    @entities.each do |model, field|
+      model.should_receive(:advanced_search)
+        .with({field => "#{search_term}:*"})
+        .and_return([])
+    end
+    Search.search(search_term)
+  end
+end
