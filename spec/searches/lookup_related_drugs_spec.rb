79d664e774c0074a27223b723e5082da8b8dfcdf
Adam Coffman <acoffman@genome.wustl.edu>
2013-10-18 14:09:02 -0500

    add lookup related drugs spec

---
+++ spec/searches/lookup_related_drugs_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe LookupRelatedDrugs do
+  it 'should find drugs that have matching names' do
+    drug = Fabricate(:drug, name: 'Turkey Sandwich')
+    results = LookupRelatedDrugs.find('Turkey')
+    results.size.should eq(1)
+    results.first.instance_variable_get("@drug").should eq(drug)
+  end
+
+  it 'should wrap the result(s) in a RelatedDrugPresenter object' do
+    Fabricate(:drug, name: 'Turkey Sandwich')
+    results = LookupRelatedDrugs.find('Turkey')
+    results.first.should be_a(RelatedDrugPresenter)
+  end
+
+  it 'should not return the same drug that was searched' do
+    Fabricate(:drug, name: 'turkey')
+    Fabricate(:drug, name: 'turkey sandwich')
+
+    results = LookupRelatedDrugs.find('turkey')
+    results.size.should eq(1)
+    results.first.instance_variable_get("@drug").name.should eq('turkey sandwich')
+  end
+
+  it 'should find drugs that have drug claims with matching names' do
+    Fabricate(:drug, name: 'turkey')
+    drug = Fabricate(:drug, name: 'sandwich meats')
+    Fabricate(:drug_claim, drugs: [drug], name: 'turkey')
+
+    results = LookupRelatedDrugs.find('turkey')
+    results.size.should eq(1)
+    results.first.instance_variable_get("@drug").should eq(drug)
+  end
+
+  it 'should find drugs that have drug claim aliases with matching names' do
+    Fabricate(:drug, name: 'turkey')
+    drug = Fabricate(:drug, name: 'sandwich meats')
+    drug_claim = Fabricate(:drug_claim, drugs: [drug], name: 'lunchable')
+    Fabricate(:drug_claim_alias, drug_claim: drug_claim, alias: 'turkey sandwich')
+    results = LookupRelatedDrugs.find('turkey')
+    results.size.should eq(1)
+    results.first.instance_variable_get("@drug").should eq(drug)
+  end
+
+  it 'should only return unique drugs' do
+    drug = Fabricate(:drug, name: 'sandwich')
+    Fabricate(:drug_claim, name: 'turkey', drugs: [drug])
+    Fabricate(:drug_claim, name: 'turkey sandwich', drugs: [drug])
+    results = LookupRelatedDrugs.find('turkey')
+    results.size.should eq(1)
+  end
+end
