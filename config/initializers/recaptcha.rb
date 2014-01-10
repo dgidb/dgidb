0a215e89fe479df55755bfda20c4a7d68a72f0b7
Adam Coffman <acoffman@genome.wustl.edu>
2014-01-10 12:18:09 -0600

    remove recaptcha

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  #Enter your own keys here, these won't work on other sites...
-  config.public_key  = '6Ld3zuESAAAAAN3CKJnsEPTi4gRUP919p9J1SGg7'
-  config.private_key = '6Ld3zuESAAAAAFMLNZehRvTha1uMXi7O5-votEH0'
-end
