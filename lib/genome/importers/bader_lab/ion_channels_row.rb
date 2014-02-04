bdc800b0f4a9e885d44d4e3d8b4c8ca07e6d5b58
Nick Spies <nspies@gscs-air.dhcp.wustl.edu>
2014-02-04 14:39:34 -0600

    Added Bader Lab Genes

---
+++ lib/genome/importers/bader_lab/ion_channels_row.rb
@@ -0,0 +1,14 @@
+require 'genome/importers/delimited_row'
+module Genome
+	module Importers
+		module BaderLab
+			class BaderLabRow < Genome::Importers::DelimitedRow
+				attribute :initial_gene_query
+				attribute :primary_name
+				attribute :entrez_gene
+				attribute :citations
+				attribute :gene_category
+			end
+		end
+	end
+end
---
+++ lib/genome/importers/bader_lab/ion_channels_tsv_importer.rb
@@ -0,0 +1,31 @@
+module Genome
+	module Importers
+		module BaderLab
+	
+	def self.source_info
+	{
+		base_url: '',
+		citation: '',
+		site_url: '',
+		source_db_version: '',
+		source_type_id: DataModel::SourceType.POTENTIALLY_DRUGGABLE,
+		source_trust_level_id:DataModel::SourceTrustLevel.EXPERT_CURATED, 
+		source_db_name: 'BaderLabGenes'
+		full_name: 'Bader Lab Genes'
+	}
+	end
+	
+	def self.run(tsv_path)
+		TSVImporter.import tsv_path, BaderLabGenesRow, source_info do
+			gene :primary_name, nomenclature: 'Entrez Gene Name' do
+				name :entrez_gene_id, nomenclature: "Entrez Gene ID"
+				attribute :initial_gene_query, name: 'Initial Gene Query'
+				attribute :citations, name: 'Counted Citations from 1950-2000'				
+				attribute :gene_category, name: 'Gene Category'
+					end
+				end
+			end.save!
+		end
+	end
+end 
+			
