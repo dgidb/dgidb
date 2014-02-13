9ff72c5ec116cfb258f92063e1be559308133647
Adam Coffman <acoffman@genome.wustl.edu>
2014-02-13 15:18:57 -0600

    add top bar news ticker for unread items

--- app/controller_filters/news_filter.rb
+++ app/controller_filters/news_filter.rb
@@ -5,10 +5,23 @@ class NewsFilter
                    else
                      true
                    end
-    controller.instance_variable_set('@unread_news', unread_news)
+
+    news_item = if unread_news
+                  most_recent_post
+                else
+                  nil
+                end
+
+    news_presenter = NewsPresenter.new(news_item, controller.view_context)
+
+    controller.instance_variable_set('@news', news_presenter)
   end
 
   def self.most_recent_post_date
     @@most_recent_post_date ||= Date.parse(EXTERNAL_STRINGS['news']['posts'].last['date'])
+  end
+
+  def self.most_recent_post
+    @@most_recent_post ||= EXTERNAL_STRINGS['news']['posts'].last
   end
 end
---
+++ app/presenters/news_presenter.rb
@@ -0,0 +1,30 @@
+class NewsPresenter < SimpleDelegator
+
+  def initialize(unread_item, view_context)
+    super(view_context)
+    @unread_item = unread_item
+  end
+
+  def news_ticker
+   if unread_news?
+     headline = link_to(@unread_item['headline'], '/news')
+
+     content_tag(:p) do
+       concat content_tag(:hr)
+       concat content_tag(:strong, 'Unread News: ')
+       concat content_tag(:strong, headline)
+       concat content_tag(:hr)
+     end
+   else
+     ''
+   end
+  end
+
+  def unread_news?
+    !!@unread_item
+  end
+
+  def unread_news_badge
+    content_tag(:span, '!', class: ['badge', 'badge-important']) if unread_news?
+  end
+end
