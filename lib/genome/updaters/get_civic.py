1d06915f9023709bfaff7373162c41b09090611d
nspies13 <nspies13@gmail.com>
2015-08-27 20:08:59 -0500

    added filtering logic

--- lib/genome/updaters/get_civic.py
+++ lib/genome/updaters/get_civic.py
@@ -24,15 +24,15 @@ for gene in resp:
         url = 'https://civic.genome.wustl.edu/api/variants/' + str(variant['id']) + '/evidence_items'
         evidence = requests.get(url).json()
         for item in evidence:
-        	if item['evidence_type'] == 'Predictive' and item['evidence_direction'] == 'Supports' and item['evidence_level'] != 'E' and item['rating'] > 2:
-				for x in item['drugs']:
-					civic_ids.append(i)
-					genes.append(gene_name)
-					drugs.append(x['name'])
-					entrez_ids.append(entrez_id)
-					interaction_type.append("N/A")
+            if item['evidence_type'] == 'Predictive' and item['evidence_direction'] == 'Supports' and item['evidence_level'] != 'E' and item['rating'] > 2:
+                for x in item['drugs']:
+                    civic_ids.append(i)
+                    genes.append(gene_name)
+                    drugs.append(x['name'])
+                    entrez_ids.append(entrez_id)
+                    interaction_type.append("N/A")
 
-with open("data/civic_dgi.tsv", "w") as f:
+with open("civic_dgi.tsv", "w+") as f:
     for i in range(len(civic_ids)):
         f.write("\t".join((str(civic_ids[i]), genes[i], str(entrez_ids[i]),
                            drugs[i], interaction_type[i])) + "\n")
