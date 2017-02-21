d8da35700b10dcc077a1a82dbafcf6e2a0a7b047
Adam Coffman <acc@fastmail.com>
2017-02-21 11:49:14 -0600

    remove outdated config option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,6 @@ DruggableGene::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   if defined?(PhusionPassenger)
     require 'phusion_passenger/rack/out_of_band_gc'
     # Trigger out-of-band GC every 5 requests.
