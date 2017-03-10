d580daede0321433253aecbf090b0368eed8e574
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-03-10 08:56:22 -0600

    Fix test to reflect actual database relations

--- spec/lib/groupers/interaction_grouper_spec.rb
+++ spec/lib/groupers/interaction_grouper_spec.rb
@@ -14,17 +14,22 @@ describe Genome::Groupers::InteractionGrouper do
     expect(DataModel::Interaction.all.count).to eq 2
   end
 
-  it 'group interaction claims with same drug and gene' do
+  it 'groups interaction claims with same drug and gene' do
     drug = Fabricate(:drug)
     gene = Fabricate(:gene)
-    drug_claim = Fabricate(:drug_claim, drug: drug)
-    gene_claim = Fabricate(:gene_claim, gene: gene)
-    Fabricate(:interaction_claim, drug_claim: drug_claim, gene_claim: gene_claim)
+    drug_claim1 = Fabricate(:drug_claim, drug: drug)
+    gene_claim1 = Fabricate(:gene_claim, gene: gene)
+    Fabricate(:interaction_claim, drug_claim: drug_claim1, gene_claim: gene_claim1)
     Genome::Groupers::InteractionGrouper.run
+    expect(DataModel::InteractionClaim.all.count).to eq 1
     expect(DataModel::Interaction.all.count).to eq 1
+
+    drug_claim2 = Fabricate(:drug_claim, drug: drug)
+    gene_claim2 = Fabricate(:gene_claim, gene: gene)
     interaction_claim_type = Fabricate(:interaction_claim_type)
-    Fabricate(:interaction_claim, drug_claim: drug_claim, gene_claim: gene_claim, interaction_claim_types: [interaction_claim_type])
+    Fabricate(:interaction_claim, drug_claim: drug_claim2, gene_claim: gene_claim2, interaction_claim_types: [interaction_claim_type])
     Genome::Groupers::InteractionGrouper.run
+    expect(DataModel::InteractionClaim.all.count).to eq 2
     expect(DataModel::Interaction.all.count).to eq 1
   end
 
