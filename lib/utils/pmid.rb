fa6f2999b6fee7897f92b0ddb507ab5a4caa38f1
Yang-Yang Feng <yanyang.f@gmail.com>
2017-04-10 16:27:08 -0500

    fixed PMID module

--- lib/utils/pmid.rb
+++ lib/utils/pmid.rb
@@ -1,31 +1,38 @@
 module PMID
-	def self.make_get_request(url)
-	  uri = URI(url)
-	  res = Net::HTTP.get_response(uri)
-	  raise StandardError.new(res.body) unless res.code == '200'
-	  res.body
-	end
-	def self.call_pubmed_api(pubmed_id)
-	  http_resp = PMID.make_get_request(PMID.url_for_pubmed_id(pubmed_id))
-	  PubMedResponse.new(http_resp)
-	end
-	def self.get_citation_from_pubmed_id(pubmed_id)
-	  resp = PMID.call_pubmed_api(pubmed_id)
-	  resp.citation
-	end
+  def self.make_get_request(url)
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    raise StandardError.new(res.body) unless res.code == '200'
+    res.body
+  end
+  def self.call_pubmed_api(pubmed_id)
+    http_resp = PMID.make_get_request(PMID.url_for_pubmed_id(pubmed_id))
+    PubMedResponse.new(http_resp)
+  end
+  def self.get_citation_from_pubmed_id(pubmed_id)
+    puts("yo")
+    resp = PMID.call_pubmed_api(pubmed_id)
+    resp.citation
+  end
 
+  def self.pubmed_url(pubmed_id)
+
+    "https://www.ncbi.nlm.nih.gov/pubmed/#{pubmed_id}"
+  end
+
+  private
   def self.url_for_pubmed_id(pubmed_id)
     "https://www.ncbi.nlm.nih.gov/pubmed/#{pubmed_id}?report=xml&format=text"
   end
 
-	class PubMedResponse
+  class PubMedResponse
     attr_reader :xml
     def initialize(response_body)
       @xml = Nokogiri::XML(Nokogiri::XML(response_body).text)
     end
 
     def citation
-      [first_author, year, journal].compact.join(', ')
+      [first_author, year, article_title, journal].compact.join(', ')
     end
 
     def authors
