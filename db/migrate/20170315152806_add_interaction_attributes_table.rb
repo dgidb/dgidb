bc231cab1477de43bd90211947c0909333daf892
Yang-Yang Feng <yanyang.f@gmail.com>
2017-04-28 14:58:44 -0500

    local data dump

---
+++ db/migrate/20170315152806_add_interaction_attributes_table.rb
@@ -0,0 +1,27 @@
+class AddInteractionAttributesTable < ActiveRecord::Migration
+  def up
+    create_table :interaction_attributes, id: false do |t|
+      t.text :id, null: false
+      t.text :interaction_id, null: false
+      t.text :name, null: false
+      t.text :value, null: false
+    end
+
+    execute 'ALTER TABLE interaction_attributes ADD PRIMARY KEY (id)'
+    execute 'ALTER TABLE interaction_attributes ADD CONSTRAINT fk_interaction FOREIGN KEY (interaction_id) REFERENCES interactions(id)'
+
+    create_table :interaction_attributes_sources, id: false do |t|
+      t.text :interaction_attribute_id, null: false
+      t.text :source_id, null: false
+    end
+
+    execute 'ALTER TABLE interaction_attributes_sources ADD PRIMARY KEY (interaction_attribute_id, source_id)'
+    execute 'ALTER TABLE interaction_attributes_sources ADD CONSTRAINT fk_interaction_attribute FOREIGN KEY (interaction_attribute_id) REFERENCES interaction_attributes(id)'
+    execute 'ALTER TABLE interaction_attributes_sources ADD CONSTRAINT fk_source FOREIGN KEY (source_id) REFERENCES sources(id)'
+  end
+
+  def down
+    drop_table :interaction_attributes_sources
+    drop_table :interaction_attributes
+  end
+end
