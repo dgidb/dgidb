d5105ea6d900375dff532733763a7d25b27deea7
Yang-Yang Feng <yanyang.f@gmail.com>
2017-04-28 15:48:14 -0500

    fix migrations uuid

--- db/migrate/20170417192246_create_interactions_publications_table.rb
+++ db/migrate/20170417192246_create_interactions_publications_table.rb
@@ -2,7 +2,7 @@ class CreateInteractionsPublicationsTable < ActiveRecord::Migration
 	def up
 		create_table :interactions_publications, id: false do |t|
 			t.text :interaction_id, null: false
-			t.integer :publication_id, null: false
+			t.text :publication_id, null: false
 		end
 		execute 'ALTER TABLE interactions_publications ADD PRIMARY KEY (interaction_id, publication_id)'
     	execute 'ALTER TABLE interactions_publications ADD CONSTRAINT fk_interaction FOREIGN KEY (interaction_id) REFERENCES interactions(id)'
--- db/migrate/20170417192258_create_interaction_claims_publications_table.rb
+++ db/migrate/20170417192258_create_interaction_claims_publications_table.rb
@@ -2,7 +2,7 @@ class CreateInteractionClaimsPublicationsTable < ActiveRecord::Migration
 	def up
 		create_table :interaction_claims_publications, id: false do |t|
 			t.text :interaction_claim_id, null: false
-			t.integer :publication_id, null: false
+			t.text :publication_id, null: false
 		end
 		execute 'ALTER TABLE interaction_claims_publications ADD PRIMARY KEY (interaction_claim_id, publication_id)'
     	execute 'ALTER TABLE interaction_claims_publications ADD CONSTRAINT fk_interaction_claim FOREIGN KEY (interaction_claim_id) REFERENCES interaction_claims(id)'
