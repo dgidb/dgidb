392ad5e1e5b1c241a2b8f5e5a0a7bbbe9d341ff7
Yang-Yang Feng <yanyang.f@gmail.com>
2017-05-10 15:26:42 -0500

    removed empty specs

--- spec/models/gene_publication_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe GenePublication, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/interactions/show.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe "interactions/show.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
