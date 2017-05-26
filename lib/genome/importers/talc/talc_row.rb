90ad49e31f5c695e5bd50594ab8b986f1b70b336
Kelsy Cotto <kelsy.cotto@wustl.edu>
2017-05-26 14:47:49 -0500

    add TALC importer

--- lib/genome/importers/talc/talc_row.rb
+++ lib/genome/importers/talc/talc_row.rb
@@ -1,25 +1,21 @@
 require 'genome/importers/delimited_row'
 module Genome
-  module Importers
-    module TALC
-      class TALCRow < Genome::Importers::DelimitedRow
-        attribute :entrez_id
-        attribute :gene_target
-        attribute :drug_name
-        attribute :interaction_type
-        attribute :drug_class
-        attribute :drug_type
-        attribute :drug_generic_name
-        attribute :drug_trade_name, Array, delimiter: ','
-        attribute :drug_synonym, Array, delimiter: ','
-        attribute :interaction_id
-        attribute :drug_cas_number
-        attribute :drug_drugbank_id
+	module Importers
+		module TALC
+			class TALCRow < Genome::Importers::DelimitedRow
+				attribute :drug_name
+				attribute :gene_target
+				attribute :interaction_type
+				attribute :drug_generic_name
+				attribute :drug_trade_name, Array, delimiter: ','
+				attribute :drug_synonym, Array, delimiter: ','
+				attribute :metadata
 
-        def valid?(opts = {})
-          true
-        end
-      end
-    end
-  end
+				def valid?(opts = {})
+			        true
+			    end
+		    end
+		end
+	end
 end
+
