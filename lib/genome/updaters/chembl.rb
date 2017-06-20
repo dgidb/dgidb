1bef1d0445388b717cd8ec5db1ff3a91d67b28b5
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-06-20 16:41:59 -0500

    fix errors in synonym importer

--- lib/genome/updaters/chembl.rb
+++ lib/genome/updaters/chembl.rb
@@ -16,16 +16,19 @@ module Genome
       end
 
       def self.mol_col_hash
-        @mol_col_hash ||= DataModel::ChemblMolecule.columns_hash
+        @@mol_col_hash ||= DataModel::ChemblMolecule.columns_hash
       end
 
       def self.syn_col_hash
-        @syn_col_hash ||= DataModel::ChemblMoleculeSynonym.columns_hash
+        @@syn_col_hash ||= DataModel::ChemblMoleculeSynonym.columns_hash
+      end
+
+      def self.connection
+        @@connection ||= PG.connect(dbname: chembl_db_name)
       end
 
       def self.init_records_from_db # This is for an initial, non-incremental load into the chembl_molecule table
         new_records = []
-        @connection = PG.connect(dbname: chembl_db_name)
         i = 0
         connection.exec( "SELECT * FROM molecule_dictionary").each do |record|
           new_records << get_params(record, mol_col_hash)
@@ -44,7 +47,9 @@ module Genome
 
         record.each do |k, v|
           ks = k.to_sym
-          if col_hash[k].type == :boolean
+          if col_hash[k].nil?
+            next
+          elsif col_hash[k].type == :boolean
             params[ks] = flag(v)
           else
             params[ks] = v
@@ -55,11 +60,13 @@ module Genome
       end
 
       def self.init_synonyms_from_db # This is for an initial, non-incremental load into chembl_molecule_synonym table
-        connection.exec("SELECT * FROM molecule_synonyms").each do |record|
-          params = get_params(record, syn_col_hash)
-          params[:synonym] = params[:synonyms]
-          synonym = DataModel::ChemblMoleculeSynonym.create(params)
-          DataModel::ChemblMolecule.where(molregno: params[:molregno]).first << synonym
+        ActiveRecord::Base.transaction do
+          connection.exec("SELECT * FROM molecule_synonyms").each do |record|
+            params = get_params(record, syn_col_hash)
+            params[:synonym] = params[:synonyms]
+            synonym = DataModel::ChemblMoleculeSynonym.create(params)
+            DataModel::ChemblMolecule.where(molregno: params[:molregno]).first.chembl_molecule_synonyms << synonym
+          end
         end
       end
     end
