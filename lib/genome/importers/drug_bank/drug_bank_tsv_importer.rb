ca58489e3dc0950919c9872fc38e8bad0ebbe133
Kelsy Cotto <kelsy.cotto@wustl.edu>
2017-06-29 01:03:35 -0500

    working version of drugbank importer

--- lib/genome/importers/drug_bank/drug_bank_tsv_importer.rb
+++ lib/genome/importers/drug_bank/drug_bank_tsv_importer.rb
@@ -2,22 +2,22 @@ module Genome
   module Importers
     module DrugBank
 
-      def self.get_version
-        File.open('lib/genome/updaters/data/version').readlines.each do |line|
-          source, version = line.split("\t")
-          if source == 'DrugBank'
-            return version.strip
-          end
-        end
-        return nil
-      end
+      # def self.get_version
+        # File.open('lib/genome/updaters/data/version').readlines.each do |line|
+          # source, version = line.split("\t")
+          # if source == 'DrugBank'
+            # return version.strip
+          # end
+        # end
+        # return nil
+      # end
 
       def self.source_info
         {
           base_url: 'http://www.drugbank.ca',
           site_url: 'http://www.drugbank.ca/',
           citation: "DrugBank 4.0: shedding new light on drug metabolism. Law V, Knox C, Djoumbou Y, Jewison T, Guo AC, Liu Y, Maciejewski A, Arndt D, Wilson M, Neveu V, Tang A, Gabriel G, Ly C, Adamjee S, Dame ZT, Han B, Zhou Y, Wishart DS.Nucleic Acids Res. 2014 Jan 1;42(1):D1091-7. PubMed ID: 24203711",
-          source_db_version: get_version,
+          source_db_version: '5.0.6',
           source_type_id: DataModel::SourceType.INTERACTION,
           source_db_name: 'DrugBank',
           full_name: 'DrugBank - Open Data Drug & Drug Target Database'
