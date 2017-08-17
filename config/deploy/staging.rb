0c804117e926ce81cfa6a103aa581bdbc4306303
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-08-17 15:05:02 -0500

    adjust precompile configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server "ec2-34-210-223-149.us-west-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
+server "ec2-34-210-223-149.us-west-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web db}
 
 set :rbenv_ruby, '2.4.1'
 
@@ -9,4 +9,3 @@ set :ssh_options, {
     forward_agent: false,
     auth_methods: %w(publickey)
 }
-
