11883f9f95ac094fbf1ad3b45426d5af7e75e633
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-08-17 00:22:19 -0500

    disable lazy precompile

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  # Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
-end
+# if defined?(Bundler)
+#   # If you precompile assets before deploying to production, use this line
+#   # Bundler.require(*Rails.groups(:assets => %w(development test)))
+#   # If you want your assets lazily compiled in production, use this line
+#   Bundler.require(:default, :assets, Rails.env)
+# end
 
 module DruggableGene
   class Application < Rails::Application
