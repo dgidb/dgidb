67d4f43ff9349603aee9a7990c2f74f09b06ae86
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-08-22 09:38:51 -0500

    Update existing API tests to also test v2

--- spec/features/api/genes_in_category_spec.rb
+++ spec/features/api/genes_in_category_spec.rb
@@ -9,22 +9,28 @@ describe 'genes_in_category' do
 
   it 'should return a 200 status code when the category exists' do
     (_, category_name) = setup_category_with_gene
-    visit "/api/v1/genes_in_category.json?category=#{category_name}"
-    expect(page.status_code).to eq(200)
+    ['v1', 'v2'].each do |version|
+      visit "/api/#{version}/genes_in_category.json?category=#{category_name}"
+      expect(page.status_code).to eq(200)
+    end
   end
 
   it 'should return a json array of genes in the category' do
     (gene, category_name) = setup_category_with_gene
-    visit "/api/v1/genes_in_category.json?category=#{category_name}"
-    body = JSON.parse(page.body)
-    expect(body).to be_an_instance_of(Array)
-    expect(body.length).to eq 1
-    expect(body.first).to eq gene.name
-    expect(page.status_code).to eq(200)
+    ['v1', 'v2'].each do |version|
+      visit "/api/#{version}/genes_in_category.json?category=#{category_name}"
+      body = JSON.parse(page.body)
+      expect(body).to be_an_instance_of(Array)
+      expect(body.length).to eq 1
+      expect(body.first).to eq gene.name
+      expect(page.status_code).to eq(200)
+    end
   end
 
   it 'should return a 404 status code when the category does not exist' do
-    visit "/api/v1/genes_in_category.json?category=blah"
-    expect(page.status_code).to eq(404)
+    ['v1', 'v2'].each do |version|
+      visit "/api/#{version}/genes_in_category.json?category=blah"
+      expect(page.status_code).to eq(404)
+    end
   end
 end
