67f647dbb61afa046cfcc40b79dc0d91890fcfeb
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-08-24 21:46:17 -0500

    fix chembl molecule table

--- lib/genome/groupers/interaction_grouper.rb
+++ lib/genome/groupers/interaction_grouper.rb
@@ -13,10 +13,7 @@ module Genome
       end
 
       def self.reset_members
-        DataModel::InteractionClaim.all.each do |interaction_claim|
-          interaction_claim.interaction = nil
-          interaction_claim.save
-        end
+        DataModel::InteractionClaim.update_all(interaction: nil)
         DataModel::InteractionAttribute.destroy_all
         DataModel::Interaction.destroy_all
       end
