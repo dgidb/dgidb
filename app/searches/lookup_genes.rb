eb9117ac1a0d64a267cbfbc63f2be30b69dc8a59
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-09-01 20:57:41 -0500

    closes #250

--- app/searches/lookup_genes.rb
+++ app/searches/lookup_genes.rb
@@ -13,23 +13,42 @@ class LookupGenes
     search_terms = search_terms.dup
     results_to_gene_groups = search_terms.each_with_object({}) { |term, h| h[term] = [] }
 
-    gene_results = filter.filter_rel(DataModel::Gene.send(scope).where(["genes.name in (?) or entrez_id in (?)", search_terms, search_terms.map {|x| Integer(x) rescue nil }.compact]))
+    unfiltered_gene_results = DataModel::Gene.send(scope).where(["genes.name in (?) or entrez_id in (?)", search_terms, search_terms.map {|x| Integer(x) rescue nil }.compact])
+    filtered_gene_results = filter.filter_rel(unfiltered_gene_results)
 
-    gene_results.each do |result|
+    ids_to_gene_groups = filtered_gene_results.each_with_object({}) { |gene, h| h[gene.id] = gene}
+
+    unfiltered_gene_results.each do |unfiltered_result|
+      result = ids_to_gene_groups[unfiltered_result.id]
+      unless result
+        result = unfiltered_result.dup
+        result.interactions = []
+        result.id = unfiltered_result.id
+      end
       if results_to_gene_groups.keys().include? result.name
         results_to_gene_groups[result.name] << result
       elsif results_to_gene_groups.keys().include? result.entrez_id.to_s
         results_to_gene_groups[result.entrez_id.to_s] << result
       end
     end
-    search_terms = search_terms - gene_results.map(&:name) - gene_results.map(&:entrez_id)
+    search_terms = search_terms - unfiltered_gene_results.map(&:name) - unfiltered_gene_results.map(&:entrez_id)
 
-    gene_alias_results = filter.filter_rel(DataModel::GeneAlias.send(scope).where(alias: search_terms))
-    gene_alias_results.each do |result|
+    unfiltered_gene_alias_results = DataModel::GeneAlias.send(scope).where(alias: search_terms)
+    filtered_gene_alias_results = filter.filter_rel(unfiltered_gene_alias_results)
+
+    ids_to_gene_groups = filtered_gene_alias_results.each_with_object({}) { |gene, h| h[gene.id] = gene}
+
+    unfiltered_gene_alias_results.each do |unfiltered_result|
+      result = ids_to_gene_groups[unfiltered_result.id]
+      unless result
+        result = unfiltered_result.dup
+        result.gene = unfiltered_result.gene.dup
+        result.gene.interactions = []
+        result.gene.id = unfiltered_result.gene.id
+        result.id = unfiltered_result.id
+      end
       results_to_gene_groups[result.alias] << result.gene
     end
-
-
 
     results_to_gene_groups
   end
