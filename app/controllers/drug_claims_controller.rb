bc7c3e19cfd5297345a493786cf53d4b25f6c473
Yang-Yang Feng <yanyang.f@gmail.com>
2017-09-04 22:50:41 -0500

    drug fix

--- app/controllers/drug_claims_controller.rb
+++ app/controllers/drug_claims_controller.rb
@@ -1,10 +1,8 @@
 class DrugClaimsController < ApplicationController
   def show
-    result = DataModel::DrugClaim.for_show
+    @drug_claim = DrugClaimPresenter.new DataModel::DrugClaim.for_show
       .where(name: params[:name].upcase)
       .where('lower(sources.source_db_name) = ?',  params[:source_db_name].downcase)
       .first || not_found("No drug claim from #{params[:source_db_name]} with name #{params[:name]} found!")
-
-      @drug_claim = DrugClaimPresenter.new(result)
   end
 end
