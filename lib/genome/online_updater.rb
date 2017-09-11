6307b5665638693aad7373647bdf9c2c72c5d936
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-09-11 09:33:11 -0500

    New online updater for Guide to Pharmacology

--- lib/genome/online_updater.rb
+++ lib/genome/online_updater.rb
@@ -25,12 +25,35 @@ module Genome
       ).first_or_create
     end
 
+    def create_drug_claim_alias(drug_claim, synonym, nomenclature)
+      DataModel::DrugClaimAlias.where(
+        alias: synonym,
+        nomenclature: nomenclature,
+        drug_claim_id: drug_claim.id,
+      ).first_or_create
+    end
+
+    def create_drug_claim_attribute(drug_claim, name, value)
+      DataModel::DrugClaimAttribute.where(
+        name: name,
+        value: value,
+        drug_claim_id: drug_claim.id
+      ).first_or_create
+    end
+
     def create_interaction_claim(gene_claim, drug_claim)
       DataModel::InteractionClaim.where(
         gene_claim_id: gene_claim.id,
         drug_claim_id: drug_claim.id,
         source_id: @source.id
       ).first_or_create
+    end
+
+    def create_interaction_claim_type(interaction_claim, type)
+      claim_type = DataModel::InteractionClaimType.where(
+        type: type
+      ).first_or_create
+      interaction_claim.interaction_claim_types << claim_type unless interaction_claim.interaction_claim_types.include? claim_type
     end
 
     def create_interaction_claim_publication(interaction_claim, pmid)
