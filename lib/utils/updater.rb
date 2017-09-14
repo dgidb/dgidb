c8db5c72f56c646fe97d49e145f8bc245036b6a0
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-09-14 16:54:16 -0500

    updater utils

---
+++ lib/utils/updater.rb
@@ -0,0 +1,66 @@
+module Utils
+  class Updater
+    attr_accessor :grouper
+    def update_all
+      import_all
+      full_regroup
+    end
+
+    def import_all
+      all_updaters.each do |updater|
+        updater_name = updater.class.to_s
+        next if updater_name == "UpdateEntrez"
+        logger.info "Processing #{updater_name}..."
+        updater.import
+      end
+      logger.info 'Imports complete!'
+    end
+
+    def full_regroup
+      logger.info 'Deleting all groups...'
+      delete_groups
+      logger.info 'Restoring gene groups (importing entrez...)'
+      restore_genes
+      logger.info 'Grouping all...'
+      group
+      logger.info 'Postgrouping...'
+      postgroup
+      logger.info 'Regroup complete!'
+    end
+
+    def all_updaters
+      if Rails.env == 'production'
+        path = File.join(%w(/var www dgidb current app jobs update_*rb))
+      else
+        path = File.join(File.dirname(Rails.root), %w(dgi-db app jobs update_*rb))
+      end
+      @all_updaters ||= Dir.glob(path).map do |file|
+        /(update_\w+)\.rb/
+            .match(file)[1]
+            .classify
+            .constantize
+            .new
+      end
+    end
+
+    def delete_groups
+      Utils::Database.delete_genes
+      Utils::Database.delete_drugs
+    end
+
+    def restore_genes
+      entrez = all_updaters.select{ |u| u.class.to_s == 'UpdateEntrez'}.first
+      entrez.import
+    end
+
+    def group
+      @grouper ||= Grouper.new
+      @grouper.perform
+    end
+
+    def postgroup
+      @postgrouper ||= PostGrouper.new
+      @postgrouper.perform
+    end
+  end
+end
