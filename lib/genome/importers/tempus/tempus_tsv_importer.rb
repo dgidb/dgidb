f75caa4c1baaf02b3260f6ef1016b9d37edffbdd
Sharon Freshour <sharonfreshour@wustl.edu>
2019-08-21 17:08:58 -0500

    new importer for tempus

---
+++ lib/genome/importers/tempus/tempus_tsv_importer.rb
@@ -0,0 +1,27 @@
+module Genome
+  module Importers
+    module Tempus
+      def self.source_info
+        {
+          #base_url:           'http://stm.sciencemag.org/content/9/383/eaag1166',
+          site_url:           'https://www.tempus.com/',
+          citation:           'https://www.tempus.com/',
+          source_db_version:  '11-November-2018',
+          source_type_id:     DataModel::SourceType.POTENTIALLY_DRUGGABLE,
+          source_trust_level_id: DataModel::SourceTrustLevel.EXPERT_CURATED,
+          source_db_name:     'Tempus',
+          full_name:          'Tempus xT'
+        }
+      end
+
+      def self.run(tsv_path)
+        TSVImporter.import tsv_path, TempusRow, source_info do
+          gene :gene_symbol, nomenclature: 'Tempus Gene Symbol' do
+            name :gene_symbol, nomenclature: 'Gene Symbol', transform: ->(x) { x.upcase }
+            category 'ClINICALLY ACTIONABLE'
+          end
+        end.save!
+      end
+    end
+  end
+end
