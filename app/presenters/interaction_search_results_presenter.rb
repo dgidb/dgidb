79fd0dfe9197f84b7cb6f98d842408ceb8978f5a
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-11-04 10:13:59 -0600

    Calculate a more comprehensive score for interaction search results

--- app/presenters/interaction_search_results_presenter.rb
+++ app/presenters/interaction_search_results_presenter.rb
@@ -35,6 +35,34 @@ class InteractionSearchResultsPresenter
     results_for_view(Maybe(grouped_results[:definite_no_interactions]))
   end
 
+  def scores_for_result(result, grouped_results)
+    if @search_context == "genes"
+      scores = result.interactions.values.flatten.each_with_object({}) do |result_interaction, h|
+        overlap_count = grouped_results.map{ |r|
+          r.interactions.values.flatten.count{|other_interaction| other_interaction.drug_id == result_interaction.drug_id}
+        }.sum
+        promiscuity_count = DataModel::Interaction.where(drug_id: result_interaction.drug_id).count
+        pub_count = result_interaction.publications.count
+        source_count = result_interaction.sources.count
+        drug_promiscuity_counts = DataModel::Interaction.group(:drug_id).count.values
+        average_drug_promiscuity = drug_promiscuity_counts.sum / drug_promiscuity_counts.size.to_f
+        h[result_interaction.id] = ((pub_count + source_count) * (overlap_count * average_drug_promiscuity / promiscuity_count)).round(2)
+      end
+    elsif @search_context == 'drugs'
+      scores = result.interactions.values.flatten.each_with_object({}) do |result_interaction, h|
+        overlap_count = grouped_results.map{ |r|
+          r.interactions.values.flatten.count{|other_interaction| other_interaction.gene_id == result_interaction.gene_id}
+        }.sum
+        promiscuity_count = DataModel::Interaction.where(gene_id: result_interaction.gene_id).count
+        pub_count = result_interaction.publications.count
+        source_count = result_interaction.sources.count
+        gene_promiscuity_counts = DataModel::Interaction.group(:gene_id).count.values
+        average_gene_promiscuity = gene_promiscuity_counts.sum / gene_promiscuity_counts.size.to_f
+        h[result_interaction.id] = ((pub_count + source_count) * (overlap_count * average_gene_promiscuity / promiscuity_count)).round(2)
+      end
+    end
+  end
+
   private
   def grouped_results
     @grouped_results ||= @search_results.group_by { |result| result.partition }
@@ -46,7 +74,8 @@ class InteractionSearchResultsPresenter
       .map{ |result| {
           term: result.search_term,
           identifiers: result.identifiers,
-          interactions: result.interactions
+          interactions: result.interactions,
+          scores: scores_for_result(result, results),
       }}
   end
 end
