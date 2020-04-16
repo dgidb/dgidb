5dab7f48a3e24af43a4c208f80cfd38337d72e85
Alex H. Wagner, PhD <awagner24@wustl.edu>
2020-04-16 17:06:31 -0500

    update cap-bundler

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development do
   gem 'rack-mini-profiler'
   gem 'rails-erd'
   gem 'capistrano', '~> 3.9.0'
+  gem 'capistrano-bundler', '~> 1.6'
   gem 'capistrano-rails', '~> 1.3'
   gem 'capistrano-rbenv', '~> 2.0'
   gem 'capistrano-passenger', '~> 0.2.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -13,3 +13,4 @@ set :migration_role, :web
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/data')
+append :linked_dirs, '.bundle'
