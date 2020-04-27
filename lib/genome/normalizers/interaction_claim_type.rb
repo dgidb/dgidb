168ff323c4f142bd5bdd676cfe9ca9e91dfccfc8
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-04-27 10:28:50 -0500

    Add step to interaction claim type normalizer to normalize existing types

--- lib/genome/normalizers/interaction_claim_type.rb
+++ lib/genome/normalizers/interaction_claim_type.rb
@@ -3,6 +3,7 @@ module Genome
     class InteractionClaimType
       def self.normalize_types
         ActiveRecord::Base.transaction do
+          normalize_existing_types
           fill_in_new_types
           cleanup_type(default_type)
           cleanup_type(other_type)
@@ -10,7 +11,25 @@ module Genome
         end
       end
 
-      
+      def self.normalize_existing_types
+        DataModel::InteractionClaimType.all.each do |ict|
+          normalized_ict = DataModel::InteractionClaimType.where(type: name_normalizer(ict.type)).first_or_create()
+          if ict != normalized_ict
+            ict.interaction_claims.each do |ic|
+              add_unless_exists(normalized_ict, ic)
+              ic.interaction_claim_types.delete(ict)
+              ic.save
+            end
+            ict.interactions.each do |i|
+              add_unless_exists_for_interaction(normalized_ict, i)
+              i.interaction_types.delete(ict)
+              i.save
+            end
+            ict.delete
+          end
+        end
+      end
+
       def self.fill_in_new_types
         existing_types = all_interaction_claim_types
         claim_type_attributes.each do |ica|
@@ -39,6 +58,12 @@ module Genome
       def self.add_unless_exists(type, interaction_claim)
         unless interaction_claim.interaction_claim_types.include?(type)
           interaction_claim.interaction_claim_types << type
+        end
+      end
+
+      def self.add_unless_exists_for_interaction(type, interaction)
+        unless interaction.interaction_types.include?(type)
+          interaction.interaction_types << type
         end
       end
 
