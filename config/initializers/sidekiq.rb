b783c7a272643e71e3e6012706c726ce9fa4c606
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-05-28 12:36:54 -0500

    Pull sidekiq credentials from secrets file if available

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,14 +7,16 @@ schedule_file = "config/scheduled_tasks.yml"
 
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end 
+end
 
-Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+Sidekiq::Web.use(Rack::Auth::Basic) do |submitted_user, submitted_password|
   # Protect against timing attacks:
   # - See https://codahale.com/a-lesson-in-timing-attacks/
   # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
   # - Use & (do not use &&) so that it doesn't short circuit.
   # - Use digests to stop length information leaking
-  ActiveSupport::SecurityUtils.secure_compare(user, ENV["SIDEKIQ_ADMIN_USER"]) &
-    ActiveSupport::SecurityUtils.secure_compare(password, ENV["SIDEKIQ_ADMIN_PASSWORD"])
+  user = ENV["SIDEKIQ_ADMIN_USER"] || Rails.application.secrets.sidekiq_admin_user
+  password = ENV["SIDEKIQ_ADMIN_PASSWORD"] || Rails.application.secrets.sidekiq_admin_password
+  ActiveSupport::SecurityUtils.secure_compare(submitted_user, user) &
+    ActiveSupport::SecurityUtils.secure_compare(submitted_password, password)
 end
