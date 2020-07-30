71314cc33f4750bef4c9584dbe607db98d397689
Adam Coffman <acoffman@wustl.edu>
2020-07-30 13:49:57 -0500

    Pull query outside loop, fix integer division

--- app/presenters/interaction_search_results_presenter.rb
+++ app/presenters/interaction_search_results_presenter.rb
@@ -41,14 +41,21 @@ class InteractionSearchResultsPresenter
     elsif @search_context == 'drugs'
       :gene_id
     end
+
+    all_promiscuity_counts = DataModel::Interaction.group(identifier)
+      .count
+      .values
+      .map{ |x| 1.0/x }
+
     scores = result.interactions.values.flatten.each_with_object({}) do |result_interaction, h|
       overlap_count = grouped_results.map{ |r|
-        r.interactions.values.flatten.count{|other_interaction| other_interaction.send(identifier) == result_interaction.send(identifier)}
+        r.interactions.values.flatten.count{ |other_interaction| other_interaction.send(identifier) == result_interaction.send(identifier) }
       }.sum
+
+
       promiscuity_count = DataModel::Interaction.where(identifier => result_interaction.send(identifier)).count
       pub_count = result_interaction.publications.size
       source_count = result_interaction.sources.size
-      all_promiscuity_counts = DataModel::Interaction.group(identifier).count.values.map{|x| 1/x}
       average_promiscuity = all_promiscuity_counts.sum / all_promiscuity_counts.size.to_f
       h[result_interaction.id] = ((pub_count + source_count) * (overlap_count * average_promiscuity / promiscuity_count)).round(2)
     end
