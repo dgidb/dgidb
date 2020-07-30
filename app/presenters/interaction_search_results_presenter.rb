42bafcd945b236a3ff843ec7e4c3e4bbcee89d35
Adam Coffman <acoffman@wustl.edu>
2020-07-30 13:58:46 -0500

    Reduce extraneous calculations and queries
     * pull calculation of all_promiscuity_scores, average promisuity outside the loop, they're
     constant between iterations
    
     * retain hash of identifier => count in promiscuity_counts variable to
     save a db query on each loop iteration. we're already loading it all
     from the db to calcuate the scores
    
     * some small formatting changes

--- app/presenters/interaction_search_results_presenter.rb
+++ app/presenters/interaction_search_results_presenter.rb
@@ -42,21 +42,25 @@ class InteractionSearchResultsPresenter
       :gene_id
     end
 
-    all_promiscuity_counts = DataModel::Interaction.group(identifier)
+    promiscuity_counts = DataModel::Interaction.group(identifier)
       .count
+
+    all_promiscuity_scores = promiscuity_counts
       .values
       .map{ |x| 1.0/x }
 
-    scores = result.interactions.values.flatten.each_with_object({}) do |result_interaction, h|
+    average_promiscuity = all_promiscuity_scores.sum / all_promiscuity_scores.size.to_f
+    result_interactions = result.interactions.values.flatten
+
+    scores = result_interactions.each_with_object({}) do |result_interaction, h|
       overlap_count = grouped_results.map{ |r|
         r.interactions.values.flatten.count{ |other_interaction| other_interaction.send(identifier) == result_interaction.send(identifier) }
       }.sum
 
+      promiscuity_count = promiscuity_counts[result_interaction.send(identifier)]
 
-      promiscuity_count = DataModel::Interaction.where(identifier => result_interaction.send(identifier)).count
       pub_count = result_interaction.publications.size
       source_count = result_interaction.sources.size
-      average_promiscuity = all_promiscuity_counts.sum / all_promiscuity_counts.size.to_f
       h[result_interaction.id] = ((pub_count + source_count) * (overlap_count * average_promiscuity / promiscuity_count)).round(2)
     end
   end
