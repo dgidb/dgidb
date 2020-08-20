5cd064824cb19fdbe9d9879fee8f73b1988f6c4c
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-08-20 08:46:25 -0500

    Update TTD importer to run on new TSV and use new-style import methods instead of legacy DSL importer

--- lib/genome/importers/ttd/ttd_tsv_importer.rb
+++ lib/genome/importers/ttd/ttd_tsv_importer.rb
@@ -1,48 +1,76 @@
-module Genome
-  module Importers
-    module TTD
-      def source_info
+require 'genome/online_updater'
+module Genome; module Importers; module TTD
+  class TTD < Genome::OnlineUpdater
+    attr_reader :file_path, :source
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def import
+      remove_existing_source
+      create_source
+      import_claims
+    end
+
+    private
+    def remove_existing_source
+      Utils::Database.delete_source('TTD')
+    end
+
+    def create_source
+      @source ||= DataModel::Source.create(
         {
           base_url:          'http://db.idrblab.net/ttd/',
           site_url:          'http://bidd.nus.edu.sg/group/cjttd/',
           citation:          "Update of TTD: Therapeutic Target Database. Zhu F, Han BC, ..., Zheng CJ, Chen YZ. Nucleic Acids Res. 38(suppl 1):D787-91, 2010. PMID: 19933260.",
-          source_db_version:  '4.3.02 (2011.08.25)',
+          source_db_version:  '2020.06.01',
           source_type_id:    DataModel::SourceType.INTERACTION,
           source_db_name:    'TTD',
           full_name:         'Therapeutic Target Database',
           license: 'Unclear. Website states "All Rights Reserved" but resource structure and description in 2002 publication indicate "open-access".',
           license_link: 'https://academic.oup.com/nar/article/30/1/412/1331814',
         }
-      end
+      )
+    end
 
-      def self.run(tsv_path)
-        na_filter = ->(x) { x.upcase == 'N/A' }
-        TSVImporter.import tsv_path, TTDRow, source_info do
-          interaction known_action_type: 'unknown' do
-            attributes :interaction_types, name: 'Interaction Type'
+    def import_claims
+      CSV.foreach(file_path, :headers => true, :col_sep => ",") do |row|
+        gene_name, gene_abbreviation = row['Target_Name'].split(' (', 2)
+        gene_abbreviation.sub!(')', '')
+        gene_claim = create_gene_claim(gene_name, 'TTD Target Name')
+        create_gene_claim_alias(gene_claim, gene_abbreviation, 'TTD Gene Abbreviation')
+        create_gene_claim_alias(gene_claim, row['TargetID'], 'TTD Target ID')
 
-            gene :target_id, nomenclature: 'TTD Partner Id' do
-              names :target_synonyms, nomenclature: 'Gene Symptom', unless: na_filter
-              name :target_uniprot_id, nomenclature: 'Uniprot Accession', unless: na_filter
-              name :target_entrez_id, nomenclature: 'Entrez Gene Id', unless: na_filter
-              name :target_ensembl_id, nomenclature: 'Ensembl Gene Id', unless: na_filter
+        drug_claim = create_drug_claim(row['Drug_Name'], row['Drug_Name'], 'TTD Drug Name')
+        create_drug_claim_alias(drug_claim, row['DrugID'], 'TTD Drug ID')
+
+        interaction_claim = create_interaction_claim(gene_claim, drug_claim)
+        if !row['MOA'].nil? and !row['MOA'] == '.'
+          create_interaction_claim_type(interaction_claim, Genome::Normalizers::InteractionClaimType.name_normalzier(row['MOA']))
+        end
+        #this is not a typo but the actual column header from the source TSV
+        if !row['Referecnce'].nil?
+          row['Referecnce'].split('; ').each do |ref|
+            if ref.include? 'pubmed'
+              url, sep, pmid = ref.rpartition('/')
+              if pmid == ''
+                pmid = url.rpartition('/').last
+              end
+              if !pmid == '217546303'
+                create_interaction_claim_publication(interaction_claim, pmid)
+              end
             end
-
-            drug :drug_id, nomenclature: 'TTD Drug Id', primary_name: :drug_name do
-              name :drug_name, nomenclature: 'Primary Drug Name'
-              name :drug_id, nomenclature: 'TTD Drug Id'
-              names :drug_synonyms, nomenclature: 'Drug Synonym', unless: na_filter
-              name :drug_cas_number, nomenclature: 'CAS Number', unless: na_filter
-              name :drug_pubchem_cid, nomenclature: 'Pubchem CId', unless: na_filter
-              name :drug_pubchem_sid, nomenclature: 'Pubchem SId', unless: na_filter
+            if ref.include? 'pmc'
+              url, sep, pmcid = ref.rpartition('/')
+              if pmcid == ''
+                pmcid = url.rpartition('/').last
+              end
+              create_interaction_claim_publication_by_pmcid(interaction_claim, pmcid)
             end
           end
-        end
-        s = DataModel::Source.find_by(source_db_name: source_info['source_db_name'])
-        s.interaction_claims.each do |ic|
-          Genome::OnlineUpdater.new.create_interaction_claim_link(ic, 'TTD Target Information', "http://idrblab.net/ttd/data/target/details/#{ic.gene_claim.name}")
+          create_interaction_claim_link(interaction_claim, 'TTD Target Information', "http://idrblab.net/ttd/data/target/details/#{row['TargetID']}")
         end
       end
     end
   end
-end
+end; end; end
