dacd053ae553a48ad9e64173bd6bcdffbbf84823
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-08-21 13:05:26 -0500

    Skip entries with a certain status

--- lib/genome/importers/ttd/ttd.rb
+++ lib/genome/importers/ttd/ttd.rb
@@ -35,6 +35,8 @@ module Genome; module Importers; module TTD
 
     def import_claims
       CSV.foreach(file_path, :headers => true, :col_sep => ",") do |row|
+        next if row['Highest_status'] == 'Terminated' || row['Highest_status'] == 'Withdrawn from market' || row['Highest status'].match(/Discontinued/)
+
         gene_name, gene_abbreviation = row['Target_Name'].split(' (', 2)
         gene_abbreviation.sub!(')', '')
         gene_claim = create_gene_claim(gene_name, 'TTD Target Name')
