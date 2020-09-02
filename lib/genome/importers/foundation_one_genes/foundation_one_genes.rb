de3570c5e0498ab134e30f61bbca20c4dc7f7f7b
Kelsy Cotto <kelsy.cotto@wustl.edu>
2020-09-02 10:28:16 -0500

    change code to use new creae_gene_claim_category

--- lib/genome/importers/foundation_one_genes/foundation_one_genes.rb
+++ lib/genome/importers/foundation_one_genes/foundation_one_genes.rb
@@ -39,7 +39,7 @@ class FoundationOneGenes < Genome::OnlineUpdater
   def create_gene_claims
     CSV.foreach(file_path, :headers => true, :col_sep => "\t") do |row|
       gene_claim = create_gene_claim(row["Gene"], 'Gene Symbol')
-      gene_claim.gene_claim_categories << 'CLINICALLY ACTIONABLE'
+      create_gene_claim_category(gene_claim, 'CLINICALLY ACTIONABLE')
     end
   end
 end
