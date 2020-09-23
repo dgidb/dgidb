340fd9352c91538ef41ae6c3b91b9cf56c1d9cc3
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-09-23 08:36:26 -0500

    Update gene grouper to optionally only work on gene claims of a specified source

--- app/jobs/grouper.rb
+++ app/jobs/grouper.rb
@@ -2,7 +2,7 @@ class Grouper < ApplicationJob
   def perform(group_genes = true, group_drugs = true, source_id = nil)
       if group_genes
         gene_grouper = Genome::Groupers::GeneGrouper.new()
-        gene_grouper.run()
+        gene_grouper.run(source_id: source_id)
       end
       if group_drugs
         drug_grouper = Genome::Groupers::DrugGrouper.new()
